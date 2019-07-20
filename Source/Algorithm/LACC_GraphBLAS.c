db1da28e78808e3c2cd0db54a5dde7d378ec4011
Bálint Hegyi <hegyi.balint.b@gmail.com>
2019-07-20 21:26:30 +0200

    Remove whitespaces

--- Source/Algorithm/LACC_GraphBLAS.c
+++ Source/Algorithm/LACC_GraphBLAS.c
@@ -299,9 +299,9 @@ GrB_Info LAGraph_lacc(GrB_Matrix A, GrB_Vector *result)
 
         GrB_eWiseMult(pchange,NULL, NULL, GrB_NE_UINT64, parents1,parents, NULL);
         GrB_reduce (&change, NULL, Lor, pchange, NULL) ;
-        // GxB_Vector_fprint(parents1, "---- parents1 ------", GxB_SHORT, stderr);
-        // GxB_Vector_fprint(parents, "---- parents ------", GxB_SHORT, stderr);
-        // GxB_Vector_fprint(pchange, "---- change ------", GxB_SHORT, stderr);
+        //GxB_Vector_fprint(parents1, "---- parents1 ------", GxB_SHORT, stderr);
+        //GxB_Vector_fprint(parents, "---- parents ------", GxB_SHORT, stderr);
+        //GxB_Vector_fprint(pchange, "---- change ------", GxB_SHORT, stderr);
     }
 
     CountCC(parents);
