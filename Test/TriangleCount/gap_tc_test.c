0e547758ed6e5f01b972d00e54fd48be7e64f2b7
Tim Davis <davis@tamu.edu>
2020-02-04 16:51:00 -0600

    tricount tests

--- Test/TriangleCount/gap_tc_test.c
+++ Test/TriangleCount/gap_tc_test.c
@@ -108,7 +108,7 @@ int main (int argc, char **argv)
     #endif
     LAGRAPH_OK (LAGraph_init ( )) ;
 
-    int ntrials = 3 ;
+    int ntrials = 1 ;
     printf ("# of trials: %d\n", ntrials) ;
 
     int nt = NTHREAD_LIST ;
