5d4ec965439c7b8b9d5b0756f5c89f8787abab54
Michel Pelletier <pelletier.michel@gmail.com>
2020-02-18 15:01:06 -0800

    test result files and approx checks.

--- Test/Complex/complextest.c
+++ Test/Complex/complextest.c
@@ -1,23 +1,46 @@
 #include "LAGraph.h"
+#include <float.h>
+#include <complex.h>
+#include <assert.h>
 
-#define OK(method)                                                          \
-{                                                                           \
-  printf(#method);                                                     \
-  printf("\n");                                                         \
-    GrB_Info this_info = method ;                                           \
-    if (! (this_info == GrB_SUCCESS || this_info == GrB_NO_VALUE))          \
-    {                                                                       \
-        printf ("complextest failure: [%d] %s\n", this_info, GrB_error ( )) ;    \
-        FREE_ALL ;                                                          \
-        return (this_info) ;                                                \
-    }                                                                       \
-}
+#define OK(method) do                                                   \
+{                                                                       \
+    GrB_Info this_info = method ;                                       \
+    if (! (this_info == GrB_SUCCESS || this_info == GrB_NO_VALUE))      \
+    {                                                                   \
+        printf ("complextest failure: [%d] %s\n", this_info, GrB_error ( )) ; \
+        FREE_ALL ;                                                      \
+        return (this_info) ;                                            \
+    }                                                                   \
+} while(0);
 
-#define FREE_ALL                    \
+#define FREE_ALL do                 \
 {                                   \
     GrB_free (&A) ;                 \
     GrB_free (&B) ;                 \
-}
+    GrB_free (&C) ;                 \
+    GrB_free (&APPROX_Complex) ;    \
+} while(0);
+
+#define FOPENR(filename) do                                     \
+{                                                               \
+    f = fopen (filename, "r") ;                                 \
+    if (f == NULL)                                              \
+    {                                                           \
+        printf ("unable to open file [%s]\n", filename) ;       \
+        FREE_ALL ;                                              \
+        return (GrB_INVALID_VALUE) ;                            \
+    }                                                           \
+} while(0) ;
+
+#define CHECK(filename) do {                        \
+    FOPENR(filename) ;                                      \
+    OK (LAGraph_mmread (&D, f)) ;                          \
+    fclose(f);                                              \
+    OK (LAGraph_isequal(&result, C, D, APPROX_Complex));  \
+    assert(result);                                         \
+} while(0) ;
+
 
 //------------------------------------------------------------------------------
 // mmtest main:
@@ -25,6 +48,17 @@
 
 // calls mmtest with a huge range of combinations of matrix sizes, types,
 // and characteristics.
+
+void complex_approx
+(
+    bool *z,
+    const double complex *x,
+    const double complex *y
+)
+{
+    *z = cabs(*x - *y) <= DBL_EPSILON;
+}
+
 
 int main (int argc, char **argv)
 {
@@ -40,12 +74,19 @@ int main (int argc, char **argv)
     LAGraph_init ( ) ;
     #endif
 
-    // Create random complex matrix
+    GrB_BinaryOp APPROX_Complex;
 
-    GrB_Matrix A = NULL, B = NULL, C = NULL;
+    GrB_Matrix A = NULL, B = NULL, C = NULL, D = NULL;
     uint64_t aseed = 42;
     uint64_t bseed = 43;
     double complex val = 3.0 + 4.0i;
+    bool result = false;
+    FILE *f = NULL ;
+
+    OK (GrB_BinaryOp_new(
+         &APPROX_Complex,
+         (LAGraph_binary_function) (&complex_approx),
+         GrB_BOOL, LAGraph_Complex, LAGraph_Complex)) ;
 
     OK(LAGraph_random
        (&A,
@@ -59,7 +100,7 @@ int main (int argc, char **argv)
         false,
         false,
         &aseed
-        ));
+        )) ;
 
     OK(LAGraph_random
        (&B,
@@ -73,31 +114,27 @@ int main (int argc, char **argv)
         false,
         false,
         &bseed
-        ));
+        )) ;
 
     OK(GrB_Matrix_new
-       (&C, LAGraph_Complex, 2, 2));
+       (&C, LAGraph_Complex, 2, 2)) ;
 
-    OK (LAGraph_mmwrite (A, stdout)) ;
-    OK (LAGraph_mmwrite (B, stdout)) ;
-    
     // Add matrices
 
     OK(GrB_eWiseAdd
-       (C, NULL, NULL, LAGraph_PLUS_Complex, A, B, NULL));
+       (C, NULL, NULL, LAGraph_PLUS_Complex, A, B, NULL)) ;
 
-    OK (LAGraph_mmwrite (C, stdout)) ;
-    
+    CHECK("data/test_eadd.mtx");
+
     /* // Multi matrices */
     OK(GrB_eWiseMult
-       (C, NULL, NULL, LAGraph_TIMES_Complex, A, B, NULL));
+       (C, NULL, NULL, LAGraph_TIMES_Complex, A, B, NULL)) ;
 
-    OK (LAGraph_mmwrite (C, stdout)) ;
-    
+    CHECK("data/test_emul.mtx");
+
     // Matrix Mult matrices
     OK(GrB_mxm
-       (C, NULL, NULL, LAGraph_PLUS_TIMES_Complex, A, B, NULL));
+       (C, NULL, NULL, LAGraph_PLUS_TIMES_Complex, A, B, NULL)) ;
 
-    OK (LAGraph_mmwrite (C, stdout)) ;
-    
+    CHECK("data/test_mxm.mtx");
 }
