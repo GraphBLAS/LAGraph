d8466cbd15cf9f9d5b9eef6589ce0f2a2a2df675
marci543 <marci543@users.noreply.github.com>
2020-05-15 16:21:01 +0200

    Use GxB_INDEX_MAX of the next SuiteSparse release

--- Include/LAGraph.h
+++ Include/LAGraph.h
@@ -1249,7 +1249,7 @@ GrB_Info LAGraph_dense_relabel   // relabel sparse IDs to dense row/column indic
     GrB_Matrix *Id2index_handle, // output matrix: A(id, index)=1 (unfilled if NULL)
     GrB_Matrix *Index2id_handle, // output matrix: B(index, id)=1 (unfilled if NULL)
     GrB_Vector *id2index_handle, // output vector: v(id)=index (unfilled if NULL)
-    const GrB_Index *ids,        // array of unique identifiers (under GB_INDEX_MAX=2^60)
+    const GrB_Index *ids,        // array of unique identifiers (under GxB_INDEX_MAX)
     GrB_Index nids,              // number of identifiers
     GrB_Index *id_dimension      // number of rows in Id2index matrix, id2index vector (unfilled if NULL)
 ) ;
