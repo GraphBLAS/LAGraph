e4c5dc49421cc1a9736d6b2db1f5dc6bd6c2b6ef
Tim Davis <DrTimothyAldenDavis@gmail.com>
2020-05-18 05:18:06 -0500

    burble on

--- Test/BetweennessCentrality/gap_bc_test.c
+++ Test/BetweennessCentrality/gap_bc_test.c
@@ -73,6 +73,7 @@ int main (int argc, char **argv)
     GrB_Vector v_batch4 = NULL ;
     GrB_Matrix SourceNodes = NULL ;
     LAGRAPH_OK (LAGraph_init ( )) ;
+    LAGRAPH_OK (GxB_set (GxB_BURBLE, true)) ;
 
     uint64_t seed = 1;
     bool tests_pass = true;
