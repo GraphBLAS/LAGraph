0ee2dc0755d43e8e4d0862cb244cef6d75ddc80b
Tim Davis <davis@tamu.edu>
2020-06-13 21:41:24 -0500

    13June2020: for HPEC20 paper

--- Test/BetweennessCentrality/gap_bc_test.c
+++ Test/BetweennessCentrality/gap_bc_test.c
@@ -34,7 +34,7 @@
 
 //------------------------------------------------------------------------------
 
-#define NTHREAD_LIST 1
+#define NTHREAD_LIST 2
 #define THREAD_LIST 0
 
 // #define NTHREAD_LIST 6
