76a5aaa599dd3ee1c0ef99d07e64e733d93ec3e4
Michel Pelletier <pelletier.michel@gmail.com>
2021-02-12 20:26:16 -0800

    untabify

--- Experimental2/Algorithm/LAGraph_Louvain_LMSP.c
+++ Experimental2/Algorithm/LAGraph_Louvain_LMSP.c
@@ -48,7 +48,7 @@ int LAGraph_Louvain_LSMP // returns -1 on failure, 0 on success
     LG_CLEAR_MSG ;
     GrB_Vector result = NULL, empty = NULL, Sj = NULL, k = NULL, v = NULL, q = NULL, t = NULL ;
     GrB_Matrix ApAT = NULL, S = NULL, W = NULL, GA = NULL;
-	GxB_Scalar max;
+    GxB_Scalar max;
 
     LG_CHECK (community_assignment == NULL, -1, "centrality is NULL") ;
     LG_CHECK (LAGraph_CheckGraph (G, msg), -1, "graph is invalid") ;
@@ -64,7 +64,7 @@ int LAGraph_Louvain_LSMP // returns -1 on failure, 0 on success
     GrB_Index *ks, *ts;
     GrB_Index kj;
 
-	GA = G->A ;
+    GA = G->A ;
 
     GrB_TRY (GrB_Matrix_nrows (&n, GA)) ;
 
@@ -74,11 +74,11 @@ int LAGraph_Louvain_LSMP // returns -1 on failure, 0 on success
     GrB_TRY (GrB_Vector_new (&v,     GrB_FP32, n)) ;
     GrB_TRY (GrB_Vector_new (&q,     GrB_FP32, n)) ;
     GrB_TRY (GrB_Vector_new (&t,     GrB_FP32, n)) ;
-	GrB_TRY (GxB_Scalar_new (&max,   GrB_FP32)) ;
-	
-	// ApAT = A.T + A
+    GrB_TRY (GxB_Scalar_new (&max,   GrB_FP32)) ;
+
+    // ApAT = A.T + A
     GrB_TRY (GrB_eWiseAdd  (ApAT, NULL, NULL, GrB_PLUS_FP32, GA, GA, GrB_DESC_T0)) ;
-	
+
     GrB_TRY (GrB_reduce (k,    NULL, NULL, GrB_PLUS_MONOID_FP32, GA, NULL)) ; // k = A.reduce_vector()
     GrB_TRY (GrB_reduce (&m,   NULL, GrB_PLUS_MONOID_FP32, k, NULL)) ;       // m = k.reduce_int()
     GrB_TRY (GrB_apply  (k,    NULL, NULL, GrB_AINV_FP32, k, NULL)) ;              // k = (-k) / m
@@ -88,28 +88,28 @@ int LAGraph_Louvain_LSMP // returns -1 on failure, 0 on success
     ks = LAGraph_Malloc (kn, sizeof (GrB_Index)) ;
 
     GrB_TRY (GrB_Vector_extractTuples (ks, NULL, &kn, k)) ;
-	
-	//     while changed and i < max_iters:
+
+    //     while changed and i < max_iters:
     for ((*iters) = 0 ; (*iters) < itermax && changed ; (*iters)++)
     {
-		changed = false ;
-		
+        changed = false ;
+
         // for j in set(k.indexes):
         for (GrB_Index j = 0; j < kn; j++) {
             kj = ks[j];
-			// Sj = S[j]
+            // Sj = S[j]
             GrB_TRY (GrB_extract  (Sj,     NULL, NULL, S, GrB_ALL, n, kj, GrB_DESC_T0)) ;
-			// S[j] = empty
-            GrB_TRY (GrB_assign   (S,      NULL, NULL, empty, kj, GrB_ALL, n, NULL)) ;     
+            // S[j] = empty
+            GrB_TRY (GrB_assign   (S,      NULL, NULL, empty, kj, GrB_ALL, n, NULL)) ;
             GrB_TRY (GrB_extract  (v,      NULL, NULL, GA, GrB_ALL, n, kj, GrB_DESC_T0)) ;
-			// v += k[j]
-            GrB_TRY (GrB_Vector_extractElement    (&f,     v, kj)) ;                                       
+            // v += k[j]
+            GrB_TRY (GrB_Vector_extractElement    (&f,     v, kj)) ;
             GrB_TRY (GrB_apply    (v,      NULL, NULL, GrB_PLUS_FP32, v, f, NULL)) ;
-			// q = v @ S
-            GrB_TRY (GrB_vxm      (q,      NULL, NULL, GrB_PLUS_TIMES_SEMIRING_FP32, v, S, NULL)) ; 
-			// t = q.select('max')
-            GrB_TRY (GrB_reduce   (&max_f, NULL, GrB_MAX_MONOID_FP32, q, NULL)) ;          
-			GrB_TRY (GxB_Scalar_setElement    (max,    max_f)) ;
+            // q = v @ S
+            GrB_TRY (GrB_vxm      (q,      NULL, NULL, GrB_PLUS_TIMES_SEMIRING_FP32, v, S, NULL)) ;
+            // t = q.select('max')
+            GrB_TRY (GrB_reduce   (&max_f, NULL, GrB_MAX_MONOID_FP32, q, NULL)) ;
+            GrB_TRY (GxB_Scalar_setElement    (max,    max_f)) ;
             GrB_TRY (GxB_select   (t,      NULL, NULL, GxB_EQ_THUNK, q, max, NULL)) ;
 
             // if t:
@@ -117,7 +117,7 @@ int LAGraph_Louvain_LSMP // returns -1 on failure, 0 on success
             if (tn == 0)
                 continue;
 
-			ts = LAGraph_Malloc (tn, sizeof (GrB_Index));
+            ts = LAGraph_Malloc (tn, sizeof (GrB_Index));
             GrB_TRY (GrB_Vector_extractTuples (ts, NULL, &tn, t)) ;
             if (tn == 1)
                 r = ts[0] ;
@@ -126,16 +126,16 @@ int LAGraph_Louvain_LSMP // returns -1 on failure, 0 on success
 
             GrB_TRY (GrB_Matrix_setElement(S, true, j, r)) ; //  S[j, r] = True
             if (GrB_Vector_extractElement(&changed, Sj, r)
-				== GrB_NO_VALUE)                             //  if Sj.get(r) is None:
+                == GrB_NO_VALUE)                             //  if Sj.get(r) is None:
                 changed = true;
-		}
-		LAGraph_FREE (ts) ;
-	}
+        }
+        LAGraph_FREE (ts) ;
+    }
 
     // S.cast(INT64).apply(INT64.POSITIONJ).reduce_vector()
     GrB_TRY (GrB_Matrix_new (&W,     GrB_INT64, n, n)) ;
-	GrB_TRY (GrB_apply  (W,      S, NULL, GxB_POSITIONJ_INT64, W, NULL)) ;
-	GrB_TRY (GrB_reduce (result, NULL, NULL, GxB_ANY_INT64, W, NULL)) ;
+    GrB_TRY (GrB_apply  (W,      S, NULL, GxB_POSITIONJ_INT64, W, NULL)) ;
+    GrB_TRY (GrB_reduce (result, NULL, NULL, GxB_ANY_INT64, W, NULL)) ;
 
     (*community_assignment) = result ;
     LAGRAPH_FREE_WORK ;
