edb51e20ab5f613d605f5295916d825759b9fa9d
Tim Davis <DrTimothyAldenDavis@gmail.com>
2021-05-11 15:42:48 -0500

    remove GxB from LAGraph_Property_ASYmmetricPattern

--- src/utility/LAGraph_Property_ASymmetricPattern.c
+++ src/utility/LAGraph_Property_ASymmetricPattern.c
@@ -11,7 +11,12 @@
 // Also computes G->AT if not already computed, if G is not an undirected
 // graph and G->A is square.
 
-#define LAGraph_FREE_WORK GrB_free(&C) ;
+#define LAGraph_FREE_WORK   \
+{                           \
+    GrB_free (&S1) ;        \
+    GrB_free (&S2) ;        \
+    GrB_free (&C) ;         \
+}
 
 #include "LG_internal.h"
 
@@ -25,7 +30,7 @@ int LAGraph_Property_ASymmetricPattern  // 0 if successful, -1 if failure
     // clear msg and check G
     //--------------------------------------------------------------------------
 
-    GrB_Matrix C = NULL ;
+    GrB_Matrix C = NULL, S1 = NULL, S2 = NULL ;
     LG_CHECK_INIT (G, msg) ;
 
     G->A_pattern_is_symmetric = LAGRAPH_UNKNOWN ;
@@ -66,9 +71,22 @@ int LAGraph_Property_ASymmetricPattern  // 0 if successful, -1 if failure
     //--------------------------------------------------------------------------
 
     GrB_TRY (GrB_Matrix_new (&C, GrB_BOOL, n, n)) ;
-    GrB_TRY (GrB_eWiseMult (C, NULL, NULL,
-        GxB_PAIR_BOOL,      // FIXME
-        A, G->AT, NULL)) ;
+
+    #if defined ( GxB_SUITESPARSE_GRAPHBLAS )
+
+        // C(i,j) = 1 if both A(i,j) and AT(i,j) exist
+        GrB_TRY (GrB_eWiseMult (C, NULL, NULL, GxB_PAIR_BOOL, A, G->AT, NULL)) ;
+
+    #else
+
+        // S1 = pattern of A, with S1(i,j)=1 if A(i,j) exists
+        LAGraph_TRY (LAGraph_Pattern (&S1, A, msg)) ;
+        // S2 = pattern of AT, with S2(i,j)=1 if AT(i,j) exists
+        LAGraph_TRY (LAGraph_Pattern (&S2, G->AT, msg)) ;
+        // C(i,j) = 1 if both S1(i,j) == 1 and S2(i,j) == 1
+        GrB_TRY (GrB_eWiseMult (C, NULL, NULL, GrB_LAND, S1, S2, NULL)) ;
+
+    #endif
 
     GrB_Index nvals1, nvals2 ;
     GrB_TRY (GrB_Matrix_nvals (&nvals1, C)) ;
