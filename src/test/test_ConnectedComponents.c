69c9a0ff64078ac95cff56791a91a598055424af
Tim Davis <DrTimothyAldenDavis@gmail.com>
2021-11-20 18:31:50 -0600

    test cc_boruvka passes

--- src/test/test_ConnectedComponents.c
+++ src/test/test_ConnectedComponents.c
@@ -14,6 +14,7 @@
 #include <acutest.h>
 
 #include <LAGraph_test.h>
+#include <LAGraphX.h>
 
 char msg [LAGRAPH_MSG_LEN] ;
 LAGraph_Graph G = NULL ;
@@ -42,6 +43,26 @@ const matrix_info files [ ] =
     {      0, "" }, 
 } ;
 
+//------------------------------------------------------------------------------
+// count_connected_components: count the # of components in a component vector
+//------------------------------------------------------------------------------
+
+int count_connected_components (GrB_Vector C) ;
+
+int count_connected_components (GrB_Vector C)
+{
+    GrB_Index n ;
+    OK (GrB_Vector_size (&n, C)) ;
+    int ncomponents = 0 ;
+    for (int i = 0 ; i < n ; i++)
+    {
+        int comp = -1 ;
+        int result = GrB_Vector_extractElement (&comp, C, i) ;
+        if (comp == i) ncomponents++ ;
+    }
+    return (ncomponents) ;
+}
+
 //----------------------------------------------------------------------------
 // test_cc_matrices: test with several matrices
 //----------------------------------------------------------------------------
@@ -56,7 +77,7 @@ void test_cc_matrices (void)
 
     LAGraph_Init (msg) ;
     GrB_Matrix A = NULL ;
-    GrB_Vector C = NULL ;
+    GrB_Vector C = NULL, C2 = NULL ;
     GrB_Type atype = NULL ;
 
     for (int k = 0 ; ; k++)
@@ -74,6 +95,8 @@ void test_cc_matrices (void)
         OK (LAGraph_MMRead (&A, &atype, f, msg)) ;
         OK (fclose (f)) ;
         TEST_MSG ("Loading of adjacency matrix failed") ;
+        GrB_Index n ;
+        OK (GrB_Matrix_nrows (&n, A)) ;
 
         // create the graph
         OK (LAGraph_New (&G, &A, atype, LAGRAPH_ADJACENCY_UNDIRECTED, msg)) ;
@@ -83,23 +106,40 @@ void test_cc_matrices (void)
         {
             // find the connected components
             OK (LAGraph_ConnectedComponents (&C, G, msg)) ;
-            OK (LAGraph_Vector_print (C, 2, stdout, msg)) ;
+            OK (LAGraph_Vector_print (C, 3, stdout, msg)) ;
 
             // count the # of connected components
-            int ncomponents = 0 ;
-            GrB_Index n ;
-            OK (GrB_Matrix_nrows (&n, G->A)) ;
-            for (int i = 0 ; i < n ; i++)
-            {
-                int comp = -1 ;
-                OK (GrB_Vector_extractElement (&comp, C, i)) ;
-                if (comp == i) ncomponents++ ;
-            }
+            int ncomponents = count_connected_components (C) ;
             printf ("# components: %6u Matrix: %s\n", ncomponents, aname) ;
             TEST_CHECK (ncomponents == ncomp) ;
 
             // check the result
             OK (LG_check_cc (C, G, msg)) ;
+
+            if (trial == 0)
+            {
+                // find the connected components with cc_boruvka
+                printf ("BORUVKA:\n") ;
+                OK (LAGraph_cc_boruvka (&C2, G->A, false)) ;
+                OK (LAGraph_Vector_print (C2, 3, stdout, msg)) ;
+                ncomponents = count_connected_components (C2) ;
+                TEST_CHECK (ncomponents == ncomp) ;
+                OK (LG_check_cc (C2, G, msg)) ;
+                OK (GrB_free (&C2)) ;
+
+                // find the connected components with cc_lacc
+                #if 0
+                printf ("LACC:\n") ;
+                OK (LAGraph_cc_lacc (&C2, G->A, false)) ;
+                // OK (LAGraph_Vector_print (C2, 3, stdout, msg)) ;
+                GxB_print (C2, 3) ;
+                ncomponents = count_connected_components (C2) ;
+                TEST_CHECK (ncomponents == ncomp) ;
+                OK (LG_check_cc (C2, G, msg)) ;
+                OK (GrB_free (&C2)) ;
+                printf ("did LACC\n") ;
+                #endif
+            }
 
             // convert to directed with symmetric pattern for next trial
             G->kind = LAGRAPH_ADJACENCY_DIRECTED ;
