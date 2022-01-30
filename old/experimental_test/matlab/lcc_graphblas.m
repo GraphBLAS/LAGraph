42f0bb062baf7dee51336dde318da5fa9485b828
Tim Davis <DrTimothyAldenDavis@gmail.com>
2022-01-30 13:32:38 -0600

    remove old/ folder

--- old/experimental_test/matlab/lcc_graphblas.m
+++
@@ -1,9 +0,0 @@
-function c = lcc_graphblas (A)
-%LCC_GRAPHBLAS compute LCC in GraphBLAS
-% c = lcc_graphblas (A)
-mwrite ('A.mtx', A) ;
-system ('./build/lcctest < A.mtx') ;
-c = load ('lcc_results') ;
-delete ('A.mtx') ;
-delete ('lcc_results') ;
-
