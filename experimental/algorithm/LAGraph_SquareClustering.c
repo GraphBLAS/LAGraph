c6a08c13a813e87a15454e75c64b4b7276c0257e
Erik Welch <erik.n.welch@gmail.com>
2022-06-23 21:07:03 -0500

    Faster: rearrange terms to avoid expensive GrB_Matrix_eWiseMult_BinaryOp
    Believe it or not, `P2 * (P2 - 1)` is actually a dominating term when I profile this,
    so, yes, it's worthwhile to re-use P2 to calculate the above as `P2 *= P2 - 1`.
    
    Also, free large objects ASAP, but peak memory usage is unchanged.

--- experimental/algorithm/LAGraph_SquareClustering.c
+++ experimental/algorithm/LAGraph_SquareClustering.c
@@ -11,7 +11,7 @@
 
 //------------------------------------------------------------------------------
 
-// TODO: summarize.
+// TODO: summarize.  This calculates `P2 = A @ A.T`, which may be very large.
 
 // https://networkx.org/documentation/stable/reference/algorithms/generated/\
 //         networkx.algorithms.cluster.square_clustering.html
@@ -26,13 +26,13 @@
     GrB_free (&denom) ;             \
     GrB_free (&D) ;                 \
     GrB_free (&P2) ;                \
-    GrB_free (&Q) ;                 \
     GrB_free (&uw_degrees) ;        \
 }
 
 #define LG_FREE_ALL                 \
 {                                   \
     LG_FREE_WORK ;                  \
+    GrB_free (&Tri) ;               \
     GrB_free (&r) ;                 \
 }
 
@@ -61,16 +61,17 @@ int LAGraph_SquareClustering
     GrB_Vector r = NULL ;
 
     // Used by pure GrB version; the largest factor of the denominator
-    GrB_Vector uw_degrees = NULL;
+    GrB_Vector uw_degrees = NULL ;
 
     // out_degrees assigned to diagonal matrix
     GrB_Matrix D = NULL ;
 
-    // plus_pair(A @ A.T).new(mask=~D.S)
+    // P2 = plus_pair(A @ A.T).new(mask=~D.S)
+    // Then used as a temporary workspace matrix (int64)
     GrB_Matrix P2 = NULL ;
 
-    // Temporary workspace matrix (int64)
-    GrB_Matrix Q = NULL ;
+    // Triangles: first(P2 & A)
+    GrB_Matrix Tri = NULL ;
 
     GrB_Vector deg = G->out_degree ;
     GrB_Matrix A = G->A ;
@@ -119,22 +120,6 @@ int LAGraph_SquareClustering
     GRB_TRY (GrB_Matrix_new (&P2, GrB_INT64, n, n)) ;
     GRB_TRY (GrB_mxm (P2, D, NULL, LAGraph_plus_one_int64, A, A, GrB_DESC_SCT1)) ;
 
-    // Now compute the number of squares (the numerator).  We cound squares
-    // based on https://arxiv.org/pdf/2007.11111.pdf (sigma_12, c_4).
-    //     Q = P2 * (P2 - 1)
-    //     squares = Q.reduce_rowwise() / 2  (and drop zeros)
-    GRB_TRY (GrB_Matrix_new (&Q, GrB_INT64, n, n)) ;
-    GRB_TRY (GrB_Matrix_apply_BinaryOp2nd_INT64 (Q, NULL, NULL, GrB_MINUS_INT64,
-        P2, 1, NULL)) ;
-    GRB_TRY (GrB_Matrix_eWiseMult_BinaryOp (Q, NULL, NULL, GrB_TIMES_INT64, Q,
-        P2, NULL)) ;
-    GRB_TRY (GrB_Vector_new (&squares, GrB_INT64, n)) ;
-    GRB_TRY (GrB_Matrix_reduce_Monoid (squares, NULL, NULL,
-        GrB_PLUS_MONOID_INT64, Q, NULL)) ;
-    // Divide by 2, and use squares as value mask to drop zeros
-    GRB_TRY (GrB_Vector_apply_BinaryOp2nd_INT64 (squares, squares, NULL,
-        GrB_DIV_INT64, squares, 2, GrB_DESC_R));
-
     // Denominator is thought of as total number of squares that could exist.
     // We use the definition from https://arxiv.org/pdf/0710.0117v1.pdf.
     // First three contributions will become negative in the final step.
@@ -147,38 +132,53 @@ int LAGraph_SquareClustering
     GRB_TRY (GrB_Vector_eWiseMult_BinaryOp (denom, NULL, NULL, GrB_TIMES_INT64,
         denom, deg, NULL)) ;
 
-    // (2) Subtract the number of squares (will become negative, so add here):
+    // (2) Subtract 1 for each edge where u-w or w-u are connected.
+    // In other words, triangles.  Use P2, since we already have it.
+    //     Tri = first(P2 & A)
+    //     denom += Tri.reduce_rowwise()
+    GRB_TRY (GrB_Matrix_new (&Tri, GrB_INT64, n, n)) ;
+    GRB_TRY (GrB_Matrix_eWiseMult_BinaryOp (Tri, NULL, NULL, GrB_FIRST_INT64, P2,
+        A, NULL)) ;
+    GRB_TRY (GrB_Matrix_reduce_Monoid (denom, NULL, GrB_PLUS_INT64,
+        GrB_PLUS_MONOID_INT64, Tri, NULL)) ;
+    GrB_free (&Tri) ;
+
+    // Now compute the number of squares (the numerator).  We count squares
+    // based on https://arxiv.org/pdf/2007.11111.pdf (sigma_12, c_4).
+    //     P2 *= P2 - 1
+    //     squares = P2.reduce_rowwise() / 2  (and drop zeros)
+    GRB_TRY (GrB_Matrix_apply_BinaryOp2nd_INT64 (P2, NULL, GrB_TIMES_INT64,
+        GrB_MINUS_INT64, P2, 1, NULL)) ;
+    GRB_TRY (GrB_Vector_new (&squares, GrB_INT64, n)) ;
+    GRB_TRY (GrB_Matrix_reduce_Monoid (squares, NULL, NULL,
+        GrB_PLUS_MONOID_INT64, P2, NULL)) ;
+    // Divide by 2, and use squares as value mask to drop zeros
+    GRB_TRY (GrB_Vector_apply_BinaryOp2nd_INT64 (squares, squares, NULL,
+        GrB_DIV_INT64, squares, 2, GrB_DESC_R)) ;
+
+    // (3) Subtract the number of squares (will become negative, so add here):
     //     denom = denom + squares
     GRB_TRY (GrB_Vector_eWiseMult_BinaryOp (denom, NULL, NULL, GrB_PLUS_INT64,
         denom, squares, NULL)) ;
-
-    // (3) Subtract 1 for each edge where u-w or w-u are connected.
-    // In other words, triangles.  Use P2, since we already have it.
-    //     Q = first(P2 & A)
-    //     denom += Q.reduce_rowwise()
-    GRB_TRY (GrB_Matrix_eWiseMult_BinaryOp (Q, NULL, NULL, GrB_FIRST_INT64, P2,
-        A, NULL)) ;
-    GRB_TRY (GrB_Matrix_reduce_Monoid (denom, NULL, GrB_PLUS_INT64,
-        GrB_PLUS_MONOID_INT64, Q, NULL)) ;
 
     // The main contribution to the denominator:
     //     degrees[u] + degrees[w] for each u-w combo.
     // This is the only positive term.
     // We subtract all other terms from this one, hence rminus.
-    //     Q = plus_pair(A @ P2.T).new(mask=A.S)
-    //     Q = any_times(Q @ D)
-    //     denom(rminus) = Q.reduce_rowwise()
-    GRB_TRY (GrB_mxm (Q, A, NULL, LAGraph_plus_one_int64, A, P2,
+    //     P2 = plus_pair(A @ P2.T).new(mask=A.S)
+    //     P2 = any_times(P2 @ D)
+    //     denom(rminus) = P2.reduce_rowwise()
+    GRB_TRY (GrB_mxm (P2, A, NULL, LAGraph_plus_one_int64, A, P2,
         GrB_DESC_RST1)) ;
-    GRB_TRY (GrB_mxm (Q, NULL, NULL, GrB_PLUS_TIMES_SEMIRING_INT64, Q, D,
+    GRB_TRY (GrB_mxm (P2, NULL, NULL, GrB_PLUS_TIMES_SEMIRING_INT64, P2, D,
         NULL)) ;
     #if LAGRAPH_SUITESPARSE
     GRB_TRY (GrB_Matrix_reduce_Monoid (denom, NULL, GxB_RMINUS_INT64,
-        GrB_PLUS_MONOID_INT64, Q, NULL)) ;
+        GrB_PLUS_MONOID_INT64, P2, NULL)) ;
     #else
     GRB_TRY (GrB_Vector_new (&uw_degrees, GrB_INT64, n)) ;
     GRB_TRY (GrB_Matrix_reduce_Monoid (uw_degrees, NULL, NULL,
-        GrB_PLUS_MONOID_INT64, Q, NULL)) ;
+        GrB_PLUS_MONOID_INT64, P2, NULL)) ;
     GRB_TRY (GrB_Vector_eWiseMult_BinaryOp (denom, NULL, NULL,
         GrB_MINUS_INT64, uw_degrees, denom, NULL)) ;
     #endif
