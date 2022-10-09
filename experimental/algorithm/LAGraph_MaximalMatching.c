aa269b03076920905ece719b62adb45e0b3fcd5f
Vidith <vidhmadh1892@gmail.com>
2022-10-09 17:38:47 -0500

    fixes in max_matching, began testing A->E

--- experimental/algorithm/LAGraph_MaximalMatching.c
+++ experimental/algorithm/LAGraph_MaximalMatching.c
@@ -73,7 +73,7 @@ int LAGraph_MaximalMatching
     GRB_TRY (GrB_Matrix_nrows (&num_nodes, E)) ;
     GRB_TRY (GrB_Matrix_ncols (&num_edges, E)) ;
     // TODO: match this type with E
-    GRB_TRY (GrB_Matrix_new (&E_t, GrB_UINT64, num_nodes, num_edges)) ;
+    GRB_TRY (GrB_Matrix_new (&E_t, GrB_UINT64, num_edges, num_nodes)) ;
     GRB_TRY (GrB_transpose (E_t, NULL, NULL, E, NULL)) ;
 
     GRB_TRY (GrB_Vector_new (&candidates, GrB_BOOL, num_edges)) ;
@@ -106,19 +106,17 @@ int LAGraph_MaximalMatching
 
     // TODO: fix semirings to match type of E. For now, let's assume E has integral edge weights.
     // for each node, counts incident edges
-    // we disallow edges that are already in the matching from contributing by using candidates vector
     GRB_TRY (GrB_mxv (node_degree, NULL, NULL, LAGraph_plus_one_uint64, E, candidates, NULL)) ;
 
     // for each edge, sums incident edges for each node. Each edge has an excess of 2 degree, but it doesn't matter since
     // we care about relative degree
-    GRB_TRY (GrB_mxv (degree, candidates, NULL, LAGraph_plus_second_uint64, E_t, node_degree, NULL)) ;
+    GRB_TRY (GrB_mxv (degree, NULL, NULL, LAGraph_plus_second_uint64, E_t, node_degree, NULL)) ;
 
     while (ncandidates > 0) {
         // first just generate the scores again
         if (matching_type == 0) {
 
             // weighs the score based on degree
-            // use RS to clear out old scores
             GRB_TRY (GrB_eWiseMult (score, candidates, NULL, GrB_DIV_FP32, Seed, degree, GrB_DESC_RS)) ;
 
         } else {
@@ -135,17 +133,22 @@ int LAGraph_MaximalMatching
         // the actual edge selection is common regardless of matching type
 
         // intermediate result. Max score edge touching each node
+        // don't need to clear this out first because we populate the result for all nodes
         GRB_TRY (GrB_mxv (max_node_neighbor, NULL, NULL, GrB_MAX_SECOND_SEMIRING_FP32, E, score, NULL)) ;
 
         // Max edge touching each candidate edge, including itself
         GRB_TRY (GrB_mxv (max_neighbor, candidates, NULL, GrB_MAX_SECOND_SEMIRING_FP32, E_t, max_node_neighbor, GrB_DESC_RS)) ;
 
+        // Note that we are using the GE operator and not G, since max_neighbor includes the self score
         GRB_TRY (GrB_eWiseAdd (new_members, candidates, NULL, GrB_GE_FP32, score, max_neighbor, GrB_DESC_R)) ;
+
         // sparsifies new_members
         GRB_TRY (GrB_select (new_members, NULL, NULL, GrB_VALUEEQ_BOOL, new_members, true, NULL)) ; 
 
         // check if any node has > 1 edge touching it. 
-        GRB_TRY (GrB_mxv (new_members_node_degree, new_members, NULL, LAGraph_plus_second_uint64, E, new_members, GrB_DESC_RS)) ;
+        // first, we need to clear out the old entries
+        GRB_TRY (GrB_assign (new_members_node_degree, NULL, NULL, empty, GrB_ALL, num_nodes, NULL)) ;
+        GRB_TRY (GrB_mxv (new_members_node_degree, NULL, NULL, LAGraph_plus_second_uint64, E, new_members, NULL)) ;
 
         GrB_Index max_degree ; 
         GRB_TRY (GrB_reduce (&max_degree, NULL, GrB_MAX_MONOID_UINT64, new_members_node_degree, NULL)) ;
@@ -163,9 +166,13 @@ int LAGraph_MaximalMatching
         // also want to remove all adjacent edges in new_members from candidates
         GRB_TRY (GrB_assign (result, new_members, NULL, true, GrB_ALL, num_edges, GrB_DESC_S)) ; 
         // to include neighbor edges, need to compute new_neighbors
+        // to do this, we need to compute the intermediate result new_members_nodes
+        // first, we need to clear out the old entries in new_members_nodes
+        GRB_TRY (GrB_assign (new_members_nodes, NULL, NULL, empty, GrB_ALL, num_nodes, NULL)) ;
         GRB_TRY (GrB_mxv (new_members_nodes, NULL, NULL, LAGraph_any_one_bool, E, new_members, NULL)) ;
         GRB_TRY (GrB_mxv (new_neighbors, NULL, NULL, LAGraph_any_one_bool, E_t, new_members_nodes, NULL)) ;
 
+        // removes the union of new_members and their neighbors
         GRB_TRY (GrB_assign (candidates, new_neighbors, NULL, empty, GrB_ALL, num_edges, GrB_DESC_S)) ;
 
         GrB_Index last_ncandidates = ncandidates ;
--- experimental/benchmark/matching_demo.c
+++ experimental/benchmark/matching_demo.c
@@ -16,26 +16,30 @@ int A_to_E(GrB_Matrix *result, const GrB_Matrix A, char *msg) {
     GrB_Index *E_values = NULL ;
 
     GrB_Index nvals ;
+    GrB_Index num_nodes ;
 
     GRB_TRY (GrB_Matrix_nvals (&nvals, A)) ;
+    GRB_TRY (GrB_Matrix_nrows (&num_nodes, A)) ;
+    GRB_TRY (GrB_Matrix_new (&E, GrB_UINT64, num_nodes, nvals / 2)) ;
     row_indices = (GrB_Index*) malloc(sizeof(GrB_Index) * nvals) ;
     col_indices = (GrB_Index*) malloc(sizeof(GrB_Index) * nvals) ;
     // TODO: need to change values to match type of A (bool, uint64, fp32, etc)
     // for now, assuming A has uint64 entries
-    values = (GrB_Index*) malloc(sizeof(GrB_UINT64) * nvals) ;
+    values = (GrB_Index*) malloc(sizeof(GrB_Index) * nvals) ;
 
     GRB_TRY (GrB_Matrix_extractTuples (row_indices, col_indices, values, &nvals, A)) ;
-
+    
     // number of entries in E should be 2 * n_edges
     // n_edges is nvals / 2 (don't count duplicates). So, number of entries in E is just nvals.
+    // TODO: again, need to change type of E_values to match type of A
     E_row_indices = (GrB_Index*) malloc(sizeof(GrB_Index) * nvals) ;
     E_col_indices = (GrB_Index*) malloc(sizeof(GrB_Index) * nvals) ;
-    E_values = (GrB_Index*) malloc(sizeof(GrB_UINT64) * nvals) ;
+    E_values = (GrB_Index*) malloc(sizeof(GrB_Index) * nvals) ;
 
     // current index in E_* arrays
     GrB_Index pos = 0;
 
-    for (int i = 0; i < nvals; i++) {
+    for (size_t i = 0; i < nvals; i++) {
         // only consider edges if row < col (prevent duplicates)
         GrB_Index row = row_indices[i] ;
         GrB_Index col = col_indices[i] ;
@@ -46,19 +50,22 @@ int A_to_E(GrB_Matrix *result, const GrB_Matrix A, char *msg) {
             E_col_indices[pos] = pos ;
             E_row_indices[pos] = row ;
             E_values[pos] = value ;
+            // printf("DBG: pos = %lld, [%lld, %lld, %lld]\n", pos, E_col_indices[pos], E_row_indices[pos], E_values[pos]);
             pos++ ;
         }
     }
 
-    for (int i = 0; i < nvals; i++){
+    for (size_t i = 0; i < nvals; i++){
         // in this pass, put the col values (2nd endpoint)
         GrB_Index row = row_indices[i] ;
         GrB_Index col = col_indices[i] ;
         GrB_Index value = values[i] ;
         if (row < col) {
-            E_col_indices[pos] = pos - nvals ;
+            // (nvals / 2) because of the (row < col) condition
+            E_col_indices[pos] = pos - (nvals / 2) ;
             E_row_indices[pos] = col ;
             E_values[pos] = value;
+            // printf("DBG: pos = %lld, [%lld, %lld, %lld]\n", pos, E_col_indices[pos], E_row_indices[pos], E_values[pos]);
             pos++ ;
         }
     }
@@ -73,12 +80,18 @@ int main (int argc, char** argv)
 {
     char msg [LAGRAPH_MSG_LEN] ;
 
+    LAGraph_Graph G = NULL ;
+    GrB_Matrix E = NULL ;
+
     bool burble = false;
     demo_init (burble) ;
 
-    char *matrix_name = (argc > 1) ? argv[1] : "stdin" ;
-
-
-
+    LAGRAPH_TRY (readproblem (&G, NULL,
+        true, true, true, NULL, false, argc, argv)) ;
+    
+    GRB_TRY (A_to_E (&E, G->A, msg)) ;
+    // LAGRAPH_TRY (LAGraph_Graph_Print (G, LAGraph_SHORT, stdout, msg)) ;
+    printf("printing E now: \n");
+    LAGRAPH_TRY (LAGraph_Matrix_Print (E, LAGraph_SHORT, stdout, msg)) ;
     return (GrB_SUCCESS) ;
 }
