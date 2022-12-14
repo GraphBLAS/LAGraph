c6a22a62267f33ab364c40c5e62871631eeb41cd
Tim Davis <DrTimothyAldenDavis@gmail.com>
2022-12-14 15:57:44 -0600

    code of conduct, changelog, and FindLAGraph

--- cmake_modules/FindLAGraph.cmake
+++ cmake_modules/FindLAGraph.cmake
@@ -23,7 +23,8 @@ This module defines the following variables:
 ::
 
   LAGRAPH_INCLUDE_DIR    - where to find LAGraph.h, etc.
-  LAGRAPH_LIBRARY        - LAGraph library
+  LAGRAPH_LIBRARY        - dynamic LAGraph library
+  LAGRAPH_STATIC         - static LAGraph library
   LAGRAPH_LIBRARIES      - List of libraries when using LAGraph.
   LAGRAPH_FOUND          - True if LAGraph found.
 
@@ -50,29 +51,70 @@ version.
 find_path(
   LAGRAPH_INCLUDE_DIR
   NAMES LAGraph.h
+  HINTS ${LAGRAPH_ROOT}
+  HINTS ENV LAGRAPH_ROOT
   HINTS ${CMAKE_SOURCE_DIR}/../LAGraph
-  PATHS LAGRAPH_ROOT ENV LAGRAPH_ROOT
   PATH_SUFFIXES include Include
   )
 
-# "build/lib" for LAGraph
-message ( STATUS "Look in " ${CMAKE_SOURCE_DIR}/../LAGraph/build )
+# dynamic LAGraph library
 find_library(
   LAGRAPH_LIBRARY
   NAMES lagraph
+  HINTS ${LAGRAPH_ROOT}
+  HINTS ENV LAGRAPH_ROOT
   HINTS ${CMAKE_SOURCE_DIR}/../LAGraph
-  PATHS LAGRAPH_ROOT ENV LAGRAPH_ROOT
   PATH_SUFFIXES lib build
   )
 
-# get version of .so using REALPATH
+if ( MSVC )
+    set ( STATIC_SUFFIX .lib )
+else ( )
+    set ( STATIC_SUFFIX .a )
+endif ( )
+
+# static LAGraph library
+set ( save ${CMAKE_FIND_LIBRARY_SUFFIXES} )
+set ( CMAKE_FIND_LIBRARY_SUFFIXES ${STATIC_SUFFIX} ${CMAKE_FIND_LIBRARY_SUFFIXES} )
+find_library ( LAGRAPH_STATIC
+  NAMES lagraph
+  HINTS ${LAGRAPH_ROOT}
+  HINTS ENV LAGRAPH_ROOT
+  HINTS ${CMAKE_SOURCE_DIR}/../LAGraph
+  PATH_SUFFIXES lib build
+  )
+set ( CMAKE_FIND_LIBRARY_SUFFIXES ${save} )
+
+# get version of the library from the dynamic library name
 get_filename_component(LAGRAPH_LIBRARY ${LAGRAPH_LIBRARY} REALPATH)
 string(
   REGEX MATCH "[0-9]+.[0-9]+.[0-9]+"
   LAGRAPH_VERSION
   ${LAGRAPH_LIBRARY}
   )
-set(LAGRAPH_LIBRARIES ${LAGRAPH_LIBRARY})
+
+if ( NOT LAGRAPH_VERSION )
+    # if the version does not appear in the filename, read the include file.
+    # The LAGraph.h file includes the following lines:
+    #   #define LAGRAPH_VERSION_MAJOR  1
+    #   #define LAGRAPH_VERSION_MINOR  0
+    #   #define LAGRAPH_VERSION_UPDATE 1
+    file ( STRINGS ${LAGRAPH_INCLUDE_DIR}/LAGraph.h LAGRAPH_VER_MAJOR_STRING
+        REGEX "define LAGRAPH_VERSION_MAJOR " )
+    file ( STRINGS ${LAGRAPH_INCLUDE_DIR}/LAGraph.h LAGRAPH_VER_MINOR_STRING
+        REGEX "define LAGRAPH_VERSION_MINOR " )
+    file ( STRINGS ${LAGRAPH_INCLUDE_DIR}/LAGraph.h LAGRAPH_VER_UPDATE_STRING
+        REGEX "define LAGRAPH_VERSION_UPDATE " )
+    message ( STATUS "major from LAGraph.h:  ${LAGRAPH_VER_MAJOR_STRING}" )
+    message ( STATUS "minor from LAGraph.h:  ${LAGRAPH_VER_MINOR_STRING}" )
+    message ( STATUS "update from LAGraph.h: ${LAGRAPH_VER_UPDATE_STRING}" )
+    string ( REGEX MATCH "[0-9]+" LAGRAPH_VER_MAJOR ${LAGRAPH_VER_MAJOR_STRING} )
+    string ( REGEX MATCH "[0-9]+" LAGRAPH_VER_MINOR ${LAGRAPH_VER_MINOR_STRING} )
+    string ( REGEX MATCH "[0-9]+" LAGRAPH_VER_UPDATE ${LAGRAPH_VER_UPDATE_STRING} )
+    set ( LAGRAPH_VERSION "${LAGRAPH_VER_MAJOR}.${LAGRAPH_VER_MINOR}.${LAGRAPH_VER_UPDATE}")
+endif ( )
+
+set ( LAGRAPH_LIBRARIES ${LAGRAPH_LIBRARY} )
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
@@ -84,13 +126,15 @@ find_package_handle_standard_args(
 mark_as_advanced(
   LAGRAPH_INCLUDE_DIR
   LAGRAPH_LIBRARY
+  LAGRAPH_STATIC
   LAGRAPH_LIBRARIES
   )
 
 if ( LAGRAPH_FOUND )
-    message ( STATUS "LAGraph include dir: " ${LAGRAPH_INCLUDE_DIR} )
-    message ( STATUS "LAGraph library:     " ${LAGRAPH_LIBRARY} )
-    message ( STATUS "LAGraph version:     " ${LAGRAPH_VERSION} )
+    message ( STATUS "LAGraph version: " ${LAGRAPH_VERSION} )
+    message ( STATUS "LAGraph include: " ${LAGRAPH_INCLUDE_DIR} )
+    message ( STATUS "LAGraph library: " ${LAGRAPH_LIBRARY} )
+    message ( STATUS "LAGraph static:: " ${LAGRAPH_STATIC} )
 else ( )
     message ( STATUS "LAGraph not found" )
 endif ( )
