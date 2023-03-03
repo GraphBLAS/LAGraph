8cf7431bc7512b0a56bfe69387a79ac2b3280e6c
Tim Davis <DrTimothyAldenDavis@gmail.com>
2023-03-03 15:31:13 -0600

    stand-alone main programs in ./mine

--- mine/test_cpp.cpp
+++ mine/test_cpp.cpp
@@ -7,11 +7,13 @@ extern "C" {
 int main(){
    char msg [1024] ;
    LAGraph_Init (msg) ;
+   LAGraph_Random_Init (msg) ;
    GrB_Matrix A ;
    // GrB_Matrix_new (&A, GrB_BOOL, 10, 10) ;
    LAGraph_Random_Matrix (&A, GrB_BOOL, 10, 10, 0.5, 42, msg) ;
    GxB_Matrix_fprint (A, "A", GxB_COMPLETE, stdout) ;
    printf("hi\n");
    GrB_Matrix_free (&A) ;
+   LAGraph_Random_Finalize (msg) ;
    LAGraph_Finalize (msg) ;
 }
