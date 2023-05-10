346b33deb4680be091abbbc32d42951989355b5c
James Smith <james.smith9113@gmail.com>
2023-05-10 12:05:14 -0400

    Undoing unnecessary formatting changes in LAGraph_Finalize.c

--- src/utility/LAGraph_Finalize.c
+++ src/utility/LAGraph_Finalize.c
@@ -19,65 +19,69 @@
 
 extern bool LG_init_has_been_called;
 
-int LAGraph_Finalize(char *msg) {
-  //--------------------------------------------------------------------------
-  // check inputs
-  //--------------------------------------------------------------------------
+int LAGraph_Finalize (char *msg)
+{
 
-  LG_CLEAR_MSG;
-  //--------------------------------------------------------------------------
-  // free global objects
-  //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    // check inputs
+    //--------------------------------------------------------------------------
 
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_first_int8));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_first_int16));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_first_int32));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_first_int64));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_first_uint8));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_first_uint16));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_first_uint32));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_first_uint64));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_first_fp32));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_first_fp64));
+    LG_CLEAR_MSG ;
 
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_second_int8));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_second_int16));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_second_int32));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_second_int64));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_second_uint8));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_second_uint16));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_second_uint32));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_second_uint64));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_second_fp32));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_second_fp64));
+    //--------------------------------------------------------------------------
+    // free global objects
+    //--------------------------------------------------------------------------
 
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_one_int8));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_one_int16));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_one_int32));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_one_int64));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_one_uint8));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_one_uint16));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_one_uint32));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_one_uint64));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_one_fp32));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_plus_one_fp64));
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_first_int8  )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_first_int16 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_first_int32 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_first_int64 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_first_uint8 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_first_uint16)) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_first_uint32)) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_first_uint64)) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_first_fp32  )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_first_fp64  )) ;
 
-  GRB_TRY(GrB_Semiring_free(&LAGraph_any_one_bool));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_any_one_int8));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_any_one_int16));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_any_one_int32));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_any_one_int64));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_any_one_uint8));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_any_one_uint16));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_any_one_uint32));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_any_one_uint64));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_any_one_fp32));
-  GRB_TRY(GrB_Semiring_free(&LAGraph_any_one_fp64));
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_second_int8  )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_second_int16 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_second_int32 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_second_int64 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_second_uint8 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_second_uint16)) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_second_uint32)) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_second_uint64)) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_second_fp32  )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_second_fp64  )) ;
 
-  //--------------------------------------------------------------------------
-  // finalize GraphBLAS
-  //--------------------------------------------------------------------------
-  GRB_TRY(GrB_finalize());
-  LG_init_has_been_called = false;
-  return (GrB_SUCCESS);
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_one_int8  )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_one_int16 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_one_int32 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_one_int64 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_one_uint8 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_one_uint16)) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_one_uint32)) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_one_uint64)) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_one_fp32  )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_plus_one_fp64  )) ;
+
+    GRB_TRY (GrB_Semiring_free (&LAGraph_any_one_bool  )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_any_one_int8  )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_any_one_int16 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_any_one_int32 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_any_one_int64 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_any_one_uint8 )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_any_one_uint16)) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_any_one_uint32)) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_any_one_uint64)) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_any_one_fp32  )) ;
+    GRB_TRY (GrB_Semiring_free (&LAGraph_any_one_fp64  )) ;
+
+    //--------------------------------------------------------------------------
+    // finalize GraphBLAS
+    //--------------------------------------------------------------------------
+
+    GRB_TRY (GrB_finalize ( )) ;
+    LG_init_has_been_called = false;
+    return (GrB_SUCCESS) ;
 }
