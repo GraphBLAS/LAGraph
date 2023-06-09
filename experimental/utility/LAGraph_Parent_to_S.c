9d4d98c845205602256e7494f29431d794286adf
Tim Davis <davis@tamu.edu>
2023-06-09 17:22:36 -0500

    draft revision of Parent_to_S

--- experimental/utility/LAGraph_Parent_to_S.c
+++ experimental/utility/LAGraph_Parent_to_S.c
@@ -63,7 +63,7 @@ int LAGraph_Parent_to_S
         GRB_TRY (GrB_Vector_new (&sorted_permutation, GrB_UINT64, n)) ;
         GRB_TRY (GxB_Vector_sort (parent_sorted, sorted_permutation, GrB_LT_UINT64, parent_cpy, NULL)) ;
         
-        GrB_Index prv ;
+        GrB_Index prv = UINT64_MAX ;
 
         // perform relabelling
         for (GrB_Index i = 0; i < n; i++) {
@@ -88,6 +88,55 @@ int LAGraph_Parent_to_S
         GRB_TRY (GrB_Matrix_new (&S, GrB_UINT64, n, n)) ;
     }
 
+    GrB_Index J = NULL, I = NULL, J_size, I_size, nvals ;
+    bool iso ;
+    GxB_Vector_unpack_CSC (parent_cpy, &J, &I, &J_size, &I_size, NULL,
+        &nvals, NULL) ;
+    GrB_Scalar one ;
+    GrB_Scalar_new (&one, GrB_UINT64) ;
+    GrB_Scalar_setElement (one, 1) ;
+    GxB_Matrix_build_Scalar (S, I, J, one, nvals, NULL) ;
+
+    LAGraph_Free ((void **) &I, NULL) ;
+    LAGraph_Free ((void **) &J, NULL) ;
+    GrB_free (&one) ;
+
+#if 0
+GrB_Info GxB_Vector_unpack_CSC  // unpack a CSC vector
+(
+    GrB_Vector v,       // vector to unpack (type and length unchanged)
+    GrB_Index **vi,     // indices
+    void **vx,          // values
+    GrB_Index *vi_size, // size of vi in bytes
+    GrB_Index *vx_size, // size of vx in bytes
+    bool *iso,          // if true, v is iso
+    GrB_Index *nvals,   // # of entries in vector
+    bool *jumbled,      // if true, indices may be unsorted
+    const GrB_Descriptor desc
+) ;
+
+GrB_Info GxB_Matrix_build_Scalar    // build a matrix from (I,J,scalar) tuples
+(
+    GrB_Matrix C,                   // matrix to build
+    const GrB_Index *I,             // array of row indices of tuples
+    const GrB_Index *J,             // array of column indices of tuples
+    GrB_Scalar scalar,              // value for all tuples
+    GrB_Index nvals                 // number of tuples
+) ;
+
+GrB_Info GrB_Matrix_build_UINT64    // build a matrix from (I,J,X) tuples
+(
+    GrB_Matrix C,                   // matrix to build
+    const GrB_Index *I,             // array of row indices of tuples
+    const GrB_Index *J,             // array of column indices of tuples
+    const uint64_t *X,              // array of values of tuples
+    GrB_Index nvals,                // number of tuples
+    const GrB_BinaryOp dup          // binary function to assemble duplicates
+) ;
+#endif
+
+
+#if 0
     for (GrB_Index idx = 0; idx < n; idx++) {
         uint64_t i ;
         GRB_TRY (GrB_Vector_extractElement (&i, parent_cpy, idx)) ;
@@ -95,6 +144,7 @@ int LAGraph_Parent_to_S
         // printf("set (%lld, %lld) to 1\n", i, j);    
         GRB_TRY (GrB_Matrix_setElement (S, 1, i, j)) ;
     }
+#endif
 
     (*result) = S ;
 
