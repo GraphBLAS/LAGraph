dae2e302e520bb7eefd1748b3c7fd7615c01c9a3
Tim Davis <davis@tamu.edu>
2024-04-17 10:05:40 -0500

    refining Markov Clustering

--- experimental/algorithm/LAGr_MarkovClustering.c
+++ experimental/algorithm/LAGr_MarkovClustering.c
@@ -17,9 +17,8 @@
 
 #define LG_FREE_WORK                                                           \
     {                                                                          \
-        GrB_free(&A);                                                          \
+        GrB_free(&T_prev);                                                     \
         GrB_free(&T);                                                          \
-        GrB_free(&T_temp);                                                     \
         GrB_free(&w);                                                          \
         GrB_free(&D);                                                          \
         GrB_free(&CC);                                                         \
@@ -28,7 +27,6 @@
         GrB_free(&argmax_v);                                                   \
         GrB_free(&argmax_p);                                                   \
         GrB_free(&zero_FP32);                                                  \
-        GrB_free(&true_BOOL);                                                  \
         LAGraph_Free((void *)&pi, NULL);                                       \
         LAGraph_Free((void *)&px, NULL);                                       \
         LAGraph_Free((void *)&pi_new, NULL);                                   \
@@ -58,24 +56,22 @@ int LAGr_MarkovClustering(
     LAGraph_Graph G,              // input graph
     char *msg)
 {
-    GrB_Matrix A = NULL;
-    GrB_Matrix T = NULL;      // transfer workspace matrix
-    GrB_Matrix T_temp = NULL; // subsequent iteration transfer matrix
+    GrB_Matrix T_prev = NULL;   // previous iteration transfer matrix
+    GrB_Matrix T = NULL;        // current iteration transfer matrix
     GrB_Matrix CC = NULL;
 
-    GrB_Vector w = NULL; // weight vector to normalize T matrix
+    GrB_Vector w = NULL; // weight vector to normalize T_* matrix
 
     GrB_Matrix D = NULL;    // diagonal workspace matrix
     GrB_Vector ones = NULL; // vector of all 1
 
-    GrB_Matrix MSE = NULL; // Mean squared error between T and T_temp (between
+    GrB_Matrix MSE = NULL; // Mean squared error between T_prev and T (between
                            // subsequent iterations)
 
     GrB_Vector argmax_v = NULL;
     GrB_Vector argmax_p = NULL;
 
     GrB_Scalar zero_FP32 = NULL;
-    GrB_Scalar true_BOOL = NULL;
 
     GrB_Index *pi = NULL, *px = NULL;
     GrB_Index *pi_new = NULL, *px_new = NULL;
@@ -94,12 +90,10 @@ int LAGr_MarkovClustering(
     GRB_TRY(GrB_Matrix_nrows(&nrows, G->A));
     GRB_TRY(GrB_Matrix_nrows(&ncols, G->A));
 
-    LG_ASSERT_MSG(nrows == ncols, -1002, "Input matrix must be square");
+    LG_ASSERT_MSG(nrows == ncols, LAGRAPH_INVALID_GRAPH,
+        "Input matrix must be square");
     n = nrows;
-
-    // All types of input matrices get cast to type FP32 for this algorithm
-    GRB_TRY(GrB_Matrix_new(&A, GrB_FP32, n, n));
-    GRB_TRY(GrB_apply(A, NULL, NULL, GrB_IDENTITY_FP32, G->A, NULL));
+    LG_ASSERT_MSG(e >= 2, GrB_INVALID_VALUE, "e must be >= 2");
 
     //--------------------------------------------------------------------------
     // initializations
@@ -112,85 +106,95 @@ int LAGr_MarkovClustering(
     GRB_TRY(GrB_Vector_new(&argmax_v, GrB_FP32, n));
     GRB_TRY(GrB_Vector_new(&argmax_p, GrB_INT64, n));
     GRB_TRY(GrB_Scalar_new(&zero_FP32, GrB_FP32));
-    GRB_TRY(GrB_Scalar_new(&true_BOOL, GrB_BOOL));
 
     GRB_TRY(GrB_Scalar_setElement(zero_FP32, 0));
-    GRB_TRY(GrB_Scalar_setElement(true_BOOL, 1));
 
     // Create identity
     GRB_TRY(GrB_assign(ones, NULL, NULL, 1, GrB_ALL, n, NULL));
     GRB_TRY(GrB_Matrix_diag(&D, ones, 0));
 
-    // Ensure all vertices have a self-edge
-    GRB_TRY(GrB_eWiseAdd(A, NULL, NULL, GrB_ONEB_FP32, A, D, NULL));
+    // All types of input matrices get cast to type FP32 for this algorithm
+    // and ensure all vertices have a self-edge
+    GRB_TRY(GrB_Matrix_new(&T, GrB_FP32, n, n));
+    GRB_TRY(GrB_eWiseAdd(T, NULL, NULL, GrB_FIRST_FP32, G->A, D, NULL));
 
-    GRB_TRY(GrB_Matrix_dup(&T_temp, A));
-    GRB_TRY(GrB_Matrix_dup(&T, T_temp));
-
-    double mse = 0;
     GrB_Index iter = 0;
     GrB_Index nvals;
 
     while (true)
     {
-        // Normalization step: Scale each column in T_temp to add up to 1
-        // w = 1 ./ sum(A(:j))
+        // Normalization step: Scale each column in T to add up to 1
+        // w = 1 ./ sum(T(:j))
         // D = diag(w)
-        GRB_TRY(GrB_reduce(w, NULL, NULL, GrB_PLUS_MONOID_FP32, T_temp,
+        GRB_TRY(GrB_reduce(w, NULL, NULL, GrB_PLUS_MONOID_FP32, T,
                            GrB_DESC_T0));
         GRB_TRY(GrB_apply(w, NULL, NULL, GrB_MINV_FP32, w, NULL));
-        if (D != NULL) GrB_free(&D);
+        GrB_free(&D);
         GRB_TRY(GrB_Matrix_diag(&D, w, 0));
-        GRB_TRY(GrB_mxm(T_temp, NULL, NULL, GrB_PLUS_TIMES_SEMIRING_FP32,
-                        T_temp, D, NULL));
+        GRB_TRY(GrB_mxm(T, NULL, NULL, GrB_PLUS_TIMES_SEMIRING_FP32,
+                        T, D, NULL));
 
-        // Compute mean squared error between subsequent iterations
-        GRB_TRY(GxB_Matrix_eWiseUnion(MSE, NULL, NULL, GrB_MINUS_FP32, T_temp,
-                                      zero_FP32, T, zero_FP32, NULL));
-        GRB_TRY(GrB_eWiseMult(MSE, NULL, NULL, GrB_TIMES_FP32, MSE, MSE, NULL));
-        GRB_TRY(GrB_reduce(&mse, NULL, GrB_PLUS_MONOID_FP32, MSE, NULL));
-        GRB_TRY(GrB_Matrix_nvals(&nvals, MSE));
-        mse /= nvals;
-
-        if (iter > max_iter || mse < convergence_threshold)
-            break;
-
-        // Set T to the previous iteration
-        if (T != NULL) GrB_free(&T);
-        GRB_TRY(GrB_Matrix_dup(&T, T_temp));
-
-        // Expansion step
-        for (int i = 0; i < e - 1; i++)
+        if (iter > 0)
         {
-            GRB_TRY(GrB_mxm(T_temp, NULL, NULL, GrB_PLUS_TIMES_SEMIRING_FP32,
-                            T_temp, T_temp, NULL));
+            // Compute mean squared error between subsequent iterations
+            double mse = 0;
+            GRB_TRY(GxB_Matrix_eWiseUnion(MSE, NULL, NULL, GrB_MINUS_FP32, T,
+                                          zero_FP32, T_prev, zero_FP32, NULL));
+//          GRB_TRY(GrB_eWiseMult(MSE, NULL, NULL, GrB_TIMES_FP32, MSE, MSE, NULL));
+            GRB_TRY(GrB_apply(MSE, NULL, NULL, GxB_POW_FP32, MSE, (double) 2, NULL));
+
+            GRB_TRY(GrB_reduce(&mse, NULL, GrB_PLUS_MONOID_FP32, MSE, NULL));
+            GRB_TRY(GrB_Matrix_nvals(&nvals, MSE));
+            mse /= nvals;
+            if (iter > max_iter || mse < convergence_threshold)
+                break;
+        }
+
+        // Set T_prev to the previous iteration
+        GrB_free(&T_prev);
+        T_prev = T ;
+        T = NULL ;
+
+        // compute the next T matrix
+        // first expansion step for i = 0
+        // T = T_prev^2
+        GRB_TRY (GrB_Matrix_new (&T, GrB_FP32, n, n)) ;
+        GRB_TRY(GrB_mxm(T, NULL, NULL, GrB_PLUS_TIMES_SEMIRING_FP32,
+                T_prev, T_prev, NULL));
+
+        // subsequent expansion steps
+        for (int i = 1; i < e - 1; i++)
+        {
+            // T = T^2
+            GRB_TRY(GrB_mxm(T, NULL, NULL, GrB_PLUS_TIMES_SEMIRING_FP32,
+                            T, T, NULL));
         }
 
         // Inflation step
         GRB_TRY(GrB_Matrix_apply_BinaryOp2nd_FP32(
-            T_temp, NULL, NULL, GxB_POW_FP32, T_temp, (double)i, NULL));
+            T, NULL, NULL, GxB_POW_FP32, T, (double)i, NULL));
 
         // Prune values less than some small threshold
-        GRB_TRY(GrB_select(T_temp, NULL, NULL, GrB_VALUEGT_FP32, T_temp,
+        GRB_TRY(GrB_select(T, NULL, NULL, GrB_VALUEGT_FP32, T,
                            pruning_threshold, NULL));
 
         iter++;
     }
 
     // In order to interpret the final iteration of the transfer matrix
-    // (T_temp), let an attractor vertex be a vertex with at least one positive
+    // (T), let an attractor vertex be a vertex with at least one positive
     // value within their corresponding row. Each attractor is attracting the
     // vertices (columns) which have positive values within its row. To compute
     // the output cluster vector, compute the argmax across columns of the
-    // steady-state T_temp matrix. Then argmax_p(i) = k means vertex i is in
+    // steady-state T matrix. Then argmax_p(i) = k means vertex i is in
     // cluster k.
 
-    // argmax_v = max (T_temp) where argmax_v(j) = max (T_temp (:,j))
-    GRB_TRY(GrB_mxv(argmax_v, NULL, NULL, GrB_MAX_FIRST_SEMIRING_FP32, T_temp,
+    // argmax_v = max (T) where argmax_v(j) = max (T (:,j))
+    GRB_TRY(GrB_mxv(argmax_v, NULL, NULL, GrB_MAX_FIRST_SEMIRING_FP32, T,
                     ones, GrB_DESC_T0));
     if (D != NULL) GrB_free(&D);
     GRB_TRY(GrB_Matrix_diag(&D, argmax_v, 0));
-    GRB_TRY(GrB_mxm(CC, NULL, NULL, GxB_ANY_EQ_FP32, T_temp, D, NULL));
+    GRB_TRY(GrB_mxm(CC, NULL, NULL, GxB_ANY_EQ_FP32, T, D, NULL));
     GRB_TRY(GrB_select(CC, NULL, NULL, GrB_VALUENE_BOOL, CC, 0, NULL));
     GRB_TRY(GrB_mxv(argmax_p, NULL, NULL, GxB_MIN_SECONDI_INT64, CC, ones,
                     GrB_DESC_T0));
@@ -203,7 +207,7 @@ int LAGr_MarkovClustering(
     GRB_TRY(GrB_Vector_extractTuples_INT64(pi, px, &p_nvals, argmax_p));
 
     // Sometimes (particularly, when the pruning threshold is high), some
-    // columns in the steady-state T_temp have no values, i.e., they are not
+    // columns in the steady-state T have no values, i.e., they are not
     // attracted to any vertex. In this case, fill in the missing values with
     // the index of the vertex (these vertices will be arbitrarily put in the
     // cluster of their index).
