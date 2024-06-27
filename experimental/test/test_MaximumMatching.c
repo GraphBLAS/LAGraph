eaf9d0e6bca4d6106e4bbc4ed08dd86c3cfbc184
kchristin <christinakoutsou22@gmail.com>
2024-06-27 15:04:52 -0500

    Parametrize tests of multiple data sets

--- experimental/test/test_MaximumMatching.c
+++ experimental/test/test_MaximumMatching.c
@@ -12,172 +12,102 @@ LAGraph_Graph G = NULL;
 #define LEN 512
 char filename[LEN + 1];
 
-void test_MCM()
+#define NTESTS 2
+
+const char *filenames[NTESTS] = {"random_weighted_bipartite2.mtx", "test_FW_2500.mtx"};
+const uint64_t spranks[NTESTS] = {298, 2009};
+
+void test_MCM(void)
 {
     LAGraph_Init(msg);
 
     // OK(LG_SET_BURBLE(1));
 
-    GrB_Matrix A = NULL;
-    snprintf(filename, LEN, LG_DATA_DIR "%s", "west0067.mtx");
-    FILE *f = fopen(filename, "r");
-    TEST_CHECK(f != NULL);
-    OK(LAGraph_MMRead(&A, f, msg));
-    OK(fclose(f));
-    GrB_Index nrows = 0, ncols = 0, nvals = 0;
-    OK(GrB_Matrix_nrows(&nrows, A));
-    OK(GrB_Matrix_ncols(&ncols, A));
-    OK(GrB_Matrix_nvals(&nvals, A));
+    for (uint64_t test = 0; test < NTESTS; test++)
+    {
 
-    // make A a bool matrix and iso-valued
-    GrB_Index *I, *J, *X;
-    double *dummy;
-    bool *iso_value;
+        GrB_Matrix A = NULL;
+        snprintf(filename, LEN, LG_DATA_DIR "%s", filenames[test]);
+        FILE *f = fopen(filename, "r");
+        TEST_CHECK(f != NULL);
+        OK(LAGraph_MMRead(&A, f, msg));
+        OK(fclose(f));
+        GrB_Index nrows = 0, ncols = 0, nvals = 0;
+        OK(GrB_Matrix_nrows(&nrows, A));
+        OK(GrB_Matrix_ncols(&ncols, A));
+        OK(GrB_Matrix_nvals(&nvals, A));
 
-    OK(LAGraph_Malloc((void **)&I, nvals, sizeof(GrB_Index), msg));
-    OK(LAGraph_Malloc((void **)&J, nvals, sizeof(GrB_Index), msg));
-    OK(LAGraph_Malloc((void **)&dummy, nvals, sizeof(double), msg));
-    OK(LAGraph_Malloc((void **)&iso_value, nvals, sizeof(bool), msg));
+        // make A a bool matrix and iso-valued
+        GrB_Index *I, *J, *X;
+        double *dummy;
+        bool *iso_value;
 
-    for (uint64_t i = 0; i < nvals; i++)
-        iso_value[i] = 1;
-    OK(GrB_Matrix_extractTuples_FP64(I, J, dummy, &nvals, A));
-    TEST_CHECK(I != NULL);
-    OK(GrB_Matrix_new(&A, GrB_BOOL, nrows, ncols));
-    OK(GrB_Matrix_build_BOOL(A, I, J, iso_value, nvals, GrB_FIRST_BOOL));
+        OK(LAGraph_Malloc((void **)&I, nvals, sizeof(GrB_Index), msg));
+        OK(LAGraph_Malloc((void **)&J, nvals, sizeof(GrB_Index), msg));
+        OK(LAGraph_Malloc((void **)&dummy, nvals, sizeof(double), msg));
+        OK(LAGraph_Malloc((void **)&iso_value, nvals, sizeof(bool), msg));
 
-    OK(LAGraph_Free((void **)&I, msg));
-    OK(LAGraph_Free((void **)&J, msg));
-    OK(LAGraph_Free((void **)&dummy, msg));
-    OK(LAGraph_Free((void **)&iso_value, msg));
+        for (uint64_t i = 0; i < nvals; i++)
+            iso_value[i] = 1;
+        OK(GrB_Matrix_extractTuples_FP64(I, J, dummy, &nvals, A));
+        TEST_CHECK(I != NULL);
+        OK(GrB_Matrix_new(&A, GrB_BOOL, nrows, ncols));
+        OK(GrB_Matrix_build_BOOL(A, I, J, iso_value, nvals, GrB_FIRST_BOOL));
 
-    OK(LAGraph_New(&G, &A, LAGraph_ADJACENCY_DIRECTED, msg));
-    TEST_CHECK(A == NULL); // A has been moved into G->A
+        OK(LAGraph_Free((void **)&I, msg));
+        OK(LAGraph_Free((void **)&J, msg));
+        OK(LAGraph_Free((void **)&dummy, msg));
+        OK(LAGraph_Free((void **)&iso_value, msg));
 
-    GrB_Vector mateC = NULL;
-    OK(GrB_Vector_new(&mateC, GrB_UINT64, ncols));
+        OK(LAGraph_New(&G, &A, LAGraph_ADJACENCY_DIRECTED, msg));
+        TEST_CHECK(A == NULL); // A has been moved into G->A
 
-    OK(LAGraph_MaximumMatching(&mateC, G, msg));
-    printf("\nmsg: %s\n", msg);
+        GrB_Vector mateC = NULL;
+        OK(GrB_Vector_new(&mateC, GrB_UINT64, ncols));
 
-    GrB_Index nmatched = 0;
+        OK(LAGraph_MaximumMatching(&mateC, G, msg));
+        printf("\nmsg: %s\n", msg);
 
-    GrB_Vector mateR = NULL;
-    OK(GrB_Vector_new(&mateR, GrB_UINT64, nrows));
+        GrB_Index nmatched = 0;
 
-    // invert to check for dups
-    GrB_Index Ibytes = 0, Jbytes = 0, Xbytes = 0;
-    bool jumbled = 1;
-    OK(GxB_Vector_unpack_CSC(mateC, (GrB_Index **)&J, (void **)&X, &Jbytes, &Xbytes, NULL, &nmatched, &jumbled, NULL));
-    OK(GrB_Vector_build_UINT64(mateR, X, J, nmatched, GrB_FIRST_UINT64));
-    GrB_Index nmateR = 0;
-    OK(GrB_Vector_nvals(&nmateR, mateR));
-    // if nvals of mateC and mateR don't match, then there's at least one row that is used in at least one matching
-    TEST_CHECK(nmatched == nmateR);
+        GrB_Vector mateR = NULL;
+        OK(GrB_Vector_new(&mateR, GrB_UINT64, nrows));
 
-    // pack matched values in a matrix
-    GrB_Matrix M = NULL;
-    A = G->A;
-    bool *val;
-    OK(LAGraph_Malloc((void **)&val, nmatched, sizeof(bool), msg));
-    for (uint64_t i = 0; i < nmatched; i++)
-        val[i] = 1;
-    OK(GrB_Matrix_new(&M, GrB_BOOL, nrows, ncols));
-    OK(GrB_Matrix_build_BOOL(M, X, J, val, nmatched, NULL));
-    OK(LAGraph_Free((void **)&val, msg));
-    // mask with matrix A to check if all edges are present in A
-    OK(GrB_Matrix_assign(M, M, NULL, A, GrB_ALL, nrows, GrB_ALL, ncols, GrB_DESC_S));
-    GrB_Index nvalsM = 0;
-    OK(GrB_Matrix_nvals(&nvalsM, M));
-    // if values have been eliminated then edges do not exist in A
-    TEST_CHECK(nvalsM == nmatched);
+        // invert to check for dups
+        GrB_Index Ibytes = 0, Jbytes = 0, Xbytes = 0;
+        bool jumbled = 1;
+        OK(GxB_Vector_unpack_CSC(mateC, (GrB_Index **)&J, (void **)&X, &Jbytes, &Xbytes, NULL, &nmatched, &jumbled, NULL));
+        OK(GrB_Vector_build_UINT64(mateR, X, J, nmatched, GrB_FIRST_UINT64));
+        GrB_Index nmateR = 0;
+        OK(GrB_Vector_nvals(&nmateR, mateR));
+        // if nvals of mateC and mateR don't match, then there's at least one row that is used in at least one matching
+        TEST_CHECK(nmatched == nmateR);
 
-    // sprank must be equal to nvals of mateC (nmatched)
-    const uint64_t matlab_res = 67;
-    TEST_CHECK(nmatched == matlab_res);
+        // pack matched values in a matrix
+        GrB_Matrix M = NULL;
+        A = G->A;
+        bool *val;
+        OK(LAGraph_Malloc((void **)&val, nmatched, sizeof(bool), msg));
+        for (uint64_t i = 0; i < nmatched; i++)
+            val[i] = 1;
+        OK(GrB_Matrix_new(&M, GrB_BOOL, nrows, ncols));
+        OK(GrB_Matrix_build_BOOL(M, X, J, val, nmatched, NULL));
+        OK(LAGraph_Free((void **)&val, msg));
+        // mask with matrix A to check if all edges are present in A
+        OK(GrB_Matrix_assign(M, M, NULL, A, GrB_ALL, nrows, GrB_ALL, ncols, GrB_DESC_S));
+        GrB_Index nvalsM = 0;
+        OK(GrB_Matrix_nvals(&nvalsM, M));
+        // if values have been eliminated then edges do not exist in A
+        TEST_CHECK(nvalsM == nmatched);
 
-    OK(LAGraph_Delete(&G, msg));
-    OK(GrB_Vector_free(&mateC));
-    OK(GrB_Vector_free(&mateR));
-    OK(GrB_Matrix_free(&M));
+        // sprank must be equal to nvals of mateC (nmatched)
+        TEST_CHECK(nmatched == spranks[test]);
 
-    // ---------------- second test -----------------
-
-    snprintf(filename, LEN, LG_DATA_DIR "%s", "zenios.mtx");
-    f = fopen(filename, "r");
-    TEST_CHECK(f != NULL);
-    OK(LAGraph_MMRead(&A, f, msg));
-    OK(fclose(f));
-    nrows = 0, ncols = 0, nvals = 0;
-    OK(GrB_Matrix_nrows(&nrows, A));
-    OK(GrB_Matrix_ncols(&ncols, A));
-    OK(GrB_Matrix_nvals(&nvals, A));
-
-    OK(LAGraph_Malloc((void **)&I, nvals, sizeof(GrB_Index), msg));
-    OK(LAGraph_Malloc((void **)&J, nvals, sizeof(GrB_Index), msg));
-    OK(LAGraph_Malloc((void **)&dummy, nvals, sizeof(double), msg));
-    OK(LAGraph_Malloc((void **)&iso_value, nvals, sizeof(bool), msg));
-
-    for (uint64_t i = 0; i < nvals; i++)
-        iso_value[i] = 1;
-    OK(GrB_Matrix_extractTuples_FP64(I, J, dummy, &nvals, A));
-    TEST_CHECK(I != NULL);
-    OK(GrB_Matrix_new(&A, GrB_BOOL, nrows, ncols));
-    OK(GrB_Matrix_build_BOOL(A, I, J, iso_value, nvals, GrB_FIRST_BOOL));
-
-    OK(LAGraph_Free((void **)&I, msg));
-    OK(LAGraph_Free((void **)&J, msg));
-    OK(LAGraph_Free((void **)&dummy, msg));
-    OK(LAGraph_Free((void **)&iso_value, msg));
-
-    OK(LAGraph_New(&G, &A, LAGraph_ADJACENCY_DIRECTED, msg));
-    TEST_CHECK(A == NULL); // A has been moved into G->A
-
-    mateC = NULL;
-    OK(GrB_Vector_new(&mateC, GrB_UINT64, ncols));
-
-    OK(LAGraph_MaximumMatching(&mateC, G, msg));
-    printf("msg: %s\n", msg);
-
-    nmatched = 0;
-
-    mateR = NULL;
-    OK(GrB_Vector_new(&mateR, GrB_UINT64, nrows));
-
-    // invert to check for dups
-    Ibytes = 0, Jbytes = 0, Xbytes = 0;
-    jumbled = 1;
-    OK(GxB_Vector_unpack_CSC(mateC, (GrB_Index **)&J, (void **)&X, &Jbytes, &Xbytes, NULL, &nmatched, &jumbled, NULL));
-    OK(GrB_Vector_build_UINT64(mateR, X, J, nmatched, GrB_FIRST_UINT64));
-    nmateR = 0;
-    OK(GrB_Vector_nvals(&nmateR, mateR));
-    // if nvals of mateC and mateR don't match, then there's at least one row that is used in at least one matching
-    TEST_CHECK(nmatched == nmateR);
-
-    // pack matched values in a matrix
-    M = NULL;
-    A = G->A;
-    OK(LAGraph_Malloc((void **)&val, nmatched, sizeof(bool), msg));
-    for (uint64_t i = 0; i < nmatched; i++)
-        val[i] = 1;
-    OK(GrB_Matrix_new(&M, GrB_BOOL, nrows, ncols));
-    OK(GrB_Matrix_build_BOOL(M, X, J, val, nmatched, NULL));
-    OK(LAGraph_Free((void **)&val, msg));
-    // mask with matrix A to check if all edges are present in A
-    OK(GrB_Matrix_assign(M, M, NULL, A, GrB_ALL, nrows, GrB_ALL, ncols, GrB_DESC_S));
-    nvalsM = 0;
-    OK(GrB_Matrix_nvals(&nvalsM, M));
-    // if values have been eliminated then edges do not exist in A
-    TEST_CHECK(nvalsM == nmatched);
-
-    // sprank must be equal to nvals of mateC (nmatched)
-    const uint64_t matlab_res_2 = 2873;
-    TEST_CHECK(nmatched == matlab_res_2);
-
-    OK(LAGraph_Delete(&G, msg));
-    OK(GrB_Vector_free(&mateC));
-    OK(GrB_Vector_free(&mateR));
-    OK(GrB_Matrix_free(&M));
+        OK(LAGraph_Delete(&G, msg));
+        OK(GrB_Vector_free(&mateC));
+        OK(GrB_Vector_free(&mateR));
+        OK(GrB_Matrix_free(&M));
+    }
 
     LAGraph_Finalize(msg);
 }
