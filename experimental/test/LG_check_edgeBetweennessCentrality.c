93d339b388058868b7ca4b442f80f91b5faa2497
peicasey <peicasey@gmail.com>
2024-11-12 14:54:58 -0600

    formatting and fixed P and result

--- experimental/test/LG_check_edgeBetweennessCentrality.c
+++ experimental/test/LG_check_edgeBetweennessCentrality.c
@@ -97,8 +97,10 @@ int test_edgeBetweenessCentrality
     // Stores dependency scores for each vertex.
     int64_t *delta = malloc(n * sizeof(int64_t)) ;
 
+    // Stack used for backtracking phase
     int64_t *S = malloc(n * sizeof(int64_t)) ;
 
+    // Queue used for BFS phase
     int64_t *queue = malloc(n * sizeof(int64_t)) ;
 
     //--------------------------------------------------------------------------
@@ -127,10 +129,9 @@ int test_edgeBetweenessCentrality
     // TODO make this a copy of A except with 1 = 0
     // A temporary result centrality matrix initialized to 0 for all vertice,
     // -- further changes would need to be made to make it a dictionary of edges.
-    int64_t *result = calloc(n * n, sizeof(int64_t));
-    result_p = malloc (n * sizeof (int64_t)) ;
-    result_j = malloc (Aj_size * sizeof (int64_t)) ;
-    result_x = calloc (Ax_size * sizeof (int64_t)) ;
+    int64_t *result_p = malloc (n * sizeof (int64_t)) ;
+    int64_t *result_j = malloc (Aj_size * sizeof (int64_t)) ;
+    int64_t *result_x = calloc (Ax_size * sizeof (int64_t)) ;
     memcpy (result_p, Ap, n * sizeof (int64_t)) ;
     memcpy (result_j, Aj, Aj_size * sizeof (int64_t)) ;
 
@@ -142,37 +143,37 @@ int test_edgeBetweenessCentrality
         // Initialize predecessors list P[w] to empty
         // TODO
         // 5. P [w] ← empty queue, ∀w ∈ V
-        Pj = malloc (n * sizeof (int64_t)) ;
-        Ptail = malloc (n * sizeof (int64_t)) ;
-        Phead = Ap ;
+        int64_t *Pj = malloc (n * sizeof (int64_t)) ;
+        int64_t *Ptail = malloc (n * sizeof (int64_t)) ;
+        int64_t *Phead = Ap ;
         memcpy (Ptail, Ap, n * sizeof (int64_t)) ;
 
         // Initialize sigma[t], d[t] for all t
         // 6. σ[t] ← 0, ∀t ∈ V , σ[s] ← 1
         // Keeps track of the number of shortest paths for each vertex.
         int64_t *sigma = calloc(n, sizeof(int64_t)) ;
-        sigma[s] = 1 ;
+        sigma [s] = 1 ;
 
         // 7. d[t] ← −1, ∀t ∈ V , d[s] ← 0
         for (size_t t = 0; t < n; t++) {
             d[t] = -1;
         }
-        d[s] = 0;
+        d [s] = 0;
 
         // Initialize queue and enqueue starting node s
         // 8. Q ← empty queue
         int64_t qh = 0, qt = 0;
         // 9. enqueue(Q, s)
-        queue[0] = s;
+        queue [0] = s;
 
         // 10. while ¬empty(Q)
         while (qh < qt) {
             // Dequeue v from Q and push onto S
             // 12. v ← dequeue(Q)
-            int64_t v = queue[qh++] ;
+            int64_t v = queue [qh++] ;
 
             // 13. push(S, v)
-            S[sp++] = v;
+            S [sp++] = v;
 
             // TODO
             // traverse all entries in A(v,:)
@@ -181,19 +182,19 @@ int test_edgeBetweenessCentrality
                 int64_t w = Aj [p] ;
                 
                 // 16. if d[w] < 0
-                if (d[w] < 0) {
+                if (d [w] < 0) {
                     // Update depth and enqueue
                     // 18. enqueue(Q, w)
-                    queue[qt++] = w;
+                    queue [qt++] = w;
                     // 19. d[w] ← d[v] + 1
-                    d[w] = d[v] + 1
+                    d [w] = d [v] + 1
                 }
 
                 // 20. if d[w] = d[v] + 1
-                if (d[w] == d[v] + 1) {
+                if (d [w] == d [v] + 1) {
                     // Update shortest path count and add predecessor
                     // 22. σ[w] ← σ[w] + σ[v]
-                    sigma[w] = sigma[w] + sigma[v]
+                    sigma [w] = sigma [w] + sigma [v]
                     // 23. append(P [w], v)
                     Pj [Ptail [w]++] = v ;
                 }
@@ -211,7 +212,7 @@ int test_edgeBetweenessCentrality
         // 25. while ¬empty(S)
         while (sp > 0) {
             // 27. w ← pop(S)
-            int64_t w = S[--sp];
+            int64_t w = S [--sp];
 
             // 28. for v ∈ P [w]
             for (int64_t p = Phead [w] ; p < Ptail [w+1] ; p++)
@@ -220,19 +221,19 @@ int test_edgeBetweenessCentrality
                 
                 // Update dependency and centrality values
                 // 30. δ[v] ← δ[v] + σ[v] × ( δ[w]/σ[w] + 1)
-                double centrality = sigma[v] * ((delta[w] / sigma[w]) + 1);
-                delta[v] += centrality;
+                double centrality = sigma[ v] * ((delta [w] / sigma [w]) + 1);
+                delta [v] += centrality;
 
                 // 31. result [(v, w)] ← result [(v, w)] + σ[v] × ( δ[w]/σ[w] + 1)
                 size_t w_i = 0;
-                for (size_t i = result_p[v]; i < result_p[v + 1]; i++) {
+                for (size_t i = result_p [v]; i < result_p [v + 1]; i++) {
                     if (result_j[i] == w) {
-                        w_i = i - result_p[v];
+                        w_i = i - result_p [v];
                         break ;
                     }
                 }
 
-                result_x[result_p[v] + w_i] += centrality;
+                result_x[result_p [v] + w_i] += centrality;
 
             }
         }
