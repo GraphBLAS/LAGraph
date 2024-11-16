922a6e42c1e23dcd71b9aa43c7f1585cf304a0d6
dperry17 <darin.peries@gmail.com>
2024-11-15 22:36:17 -0600

    created semiring

--- experimental/algorithm/LAGraph_MaxFlow.c
+++ experimental/algorithm/LAGraph_MaxFlow.c
@@ -1,23 +1,39 @@
 
-#define LG_FREE_WORK                        \
-{                                           \
-    /* free any workspace used here */      \
-    GrB_free (&W) ;                         \
-}
-
-#define LG_FREE_ALL                         \
-{                                           \
-    /* free any workspace used here */      \
-    LG_FREE_WORK ;                          \
-    /* free all the output variable(s) */   \
-    GrB_free (&Y) ;                         \
-    /* take any other corrective action */  \
-}
-
 #include <LG_internal.h>
 #include <LAGraphX.h>
 #include <LAGraph.h>
 
+#undef LG_FREE_ALL
+#undef LG_FREE_WORK
+
+#define LG_FREE_WORK                        \
+{                                           \
+  GrB_Type_free(&GrB_ResidualEdge);         \
+}                                           \
+
+#define LG_FREE_ALL                                 \
+{                                                   \
+    LG_FREE_WORK ;                                  \
+}
+
+//casting for unary ops
+#define F_UNARY(f) ((void (*)(void *, const void *))f)
+
+// casting for index binary ops
+#define F_INDEX_BINARY(f) ((void (*)(void*, const void*, GrB_Index, GrB_Index, const void *, GrB_Index, GrB_Index, const void *)) f)
+
+// casting for binary op
+#define F_BINARY(f) ((void (*)(void *, const void *, const void *)) f)
+
+// strings for JIT
+#define GRB_FLOWEDGE_STR "Typedef struct{ float flow; float capacity; } GrB_Flow_Edge;"
+#define GRB_MFRESULT_STR "typedef struct{ float residual; int d; GrB_Index j; } MF_result_tuple; "
+#define GRB_CRF_STR "void CreateResidualForward_UOp(GrB_Flow_Edge *f, const float *cap) {f->flow = 0; f->capacity = (*cap);}"
+#define GRB_CRB_STR "void CreateResidualBackward_UOp(GrB_Flow_Edge *f, const float *cap) {f->flow = 0; f->capacity = (*cap);}"
+#define GZB_MULT_STR "void Rxd_MultBOp(MF_result_tuple *y, const GrB_Flow_Edge *R, GrB_Index ix, GrB_Index jx, const int *d, GrB_Index iy, GrB_Index ik, const int* theta) { float r = R->capacity - R->flow; if(r > 0){ y->residual = r; y->d = *d; y->j = jx; } else{ y->residual = 0; y->d = INT32_MAX; y->j = -1; } }"
+#define GRB_ADD_STR "void Rxd_AddMonoid(MF_result_tuple * z, const MF_result_tuple * x, const MF_result_tuple * y) {if(x->d < y->d){ memcpy(z, x, sizeof(MF_result_tuple)); } else if(x->d > y->d){ memcpy(z, y, sizeof(MF_result_tuple));}else{if(x->residual > y->residual){ memcpy(z, x, sizeof(MF_result_tuple)); } else if(x->residual < y->residual){ memcpy(z, y, sizeof(MF_result_tuple)); } else{ if(x->j > y->j){ memcpy(z, x, sizeof(MF_result_tuple)); } else{ memcpy(z, y, sizeof(MF_result_tuple)); } } } }"
+
+//custom types
 typedef struct{
   float flow;
   float capacity;
@@ -45,12 +61,12 @@ void CreateResidualBackward_UOp(GrB_Flow_Edge *f, const float *cap) {
 
 //TO-DO: create add operation for flow edges.
 
-void Rxd_MultBOp(MF_result_tuple *y, GrB_Flow_Edge *R, int *d, int i, int j) {
+void Rxd_MultBOp(MF_result_tuple *y, const GrB_Flow_Edge *R, GrB_Index ix, GrB_Index jx, const int *d, GrB_Index iy, GrB_Index ik, const int* theta) {
   float r = R->capacity - R->flow;
   if(r > 0){
     y->residual = r;
     y->d = *d;
-    y->j = j;
+    y->j = jx;
   }
   else{
     y->residual = 0;
@@ -59,7 +75,7 @@ void Rxd_MultBOp(MF_result_tuple *y, GrB_Flow_Edge *R, int *d, int i, int j) {
   }
 }
 
-void Rxd_AddMonoid(MF_result_tuple * z, MF_result_tuple * x, MF_result_tuple * y) {
+void Rxd_AddMonoid(MF_result_tuple * z, const MF_result_tuple * x, const MF_result_tuple * y) {
   if(x->d < y->d){
     memcpy(z, x, sizeof(MF_result_tuple));
   }
@@ -88,6 +104,39 @@ void Rxd_AddMonoid(MF_result_tuple * z, MF_result_tuple * x, MF_result_tuple * y
 // f is max flow
 int LAGraph_MaxFlow(LAGraph_Graph *G, GrB_Index S, GrB_Index T, GrB_Matrix *R, int * f, char *msg){
   
-  
+  //create semirings and types
+  GrB_Type GrB_ResidualEdge;
+  GRB_TRY(GxB_Type_new(&GrB_ResidualEdge, sizeof(GrB_Flow_Edge), "GrB_Flow_Edge", GRB_FLOWEDGE_STR));
+
+  GrB_Type GrB_MFResult;
+  GRB_TRY(GxB_Type_new(&GrB_MFResult, sizeof(MF_result_tuple), "MF_result_tuple", GRB_MFRESULT_STR));
+
+  GrB_UnaryOp GrB_CRF_UOp;
+  GRB_TRY(GxB_UnaryOp_new(&GrB_CRF_UOp, F_UNARY(CreateResidualForward_UOp), GrB_ResidualEdge, GrB_FP32, "CreateResidualForward_UOp", GRB_CRF_STR));
+
+  GrB_UnaryOp GrB_CRB_UOp;
+  GRB_TRY(GxB_UnaryOp_new(&GrB_CRB_UOp, F_UNARY(CreateResidualBackward_UOp), GrB_ResidualEdge, GrB_FP32, "CreateResidualForward_UOp", GRB_CRB_STR));
+
+  GzB_IndexBinaryOp GzB_MFMult;
+  GrB_BinaryOp GrB_RxdMult;
+  GrB_Scalar unused_theta;
+  GRB_TRY(GrB_Scalar_new(&unused_theta, GrB_INT32));
+  GRB_TRY(GrB_Scalar_setElement_INT32(unused_theta, 0));
+  GRB_TRY(GzB_IndexBinaryOp_new(&GzB_MFMult, F_INDEX_BINARY(Rxd_MultBOp), GrB_MFResult, GrB_ResidualEdge, GrB_INT32, GrB_INT32,  "Rxd_MultBOp", GZB_MULT_STR));
+  GRB_TRY(GzB_BinaryOp_new_IndexOp(&GrB_RxdMult, GzB_MFMult, unused_theta));
+
+  GrB_BinaryOp GrB_MFAdd;
+  GrB_Monoid GrB_RxdAdd;
+  GRB_TRY(GxB_BinaryOp_new(&GrB_MFAdd, F_BINARY(Rxd_AddMonoid), GrB_MFResult, GrB_MFResult, GrB_MFResult, "Rxd_AddMonoid", GRB_ADD_STR));
+  MF_result_tuple id;
+  id.d = INT32_MAX;
+  id.j = -1;
+  id.residual = 0;
+  GRB_TRY(GrB_Monoid_new(&GrB_RxdAdd, GrB_MFAdd, (void*)&id));
+
+  GrB_Semiring Rxd_semiring;
+  GRB_TRY(GrB_Semiring_new(&Rxd_semiring, GrB_RxdAdd, GrB_RxdMult));
+ 
+  LG_FREE_ALL;
   return 0;
 }
