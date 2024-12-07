e5a8108b424a5c9486586dc771941c5f18c4cfa4
Gabriel Gomez <gomezg@tamu.edu>
2024-12-07 17:11:22 -0600

    Added CSC magic to testing

--- experimental/benchmark/speed_hash_demo.c
+++ experimental/benchmark/speed_hash_demo.c
@@ -41,11 +41,12 @@ int main (int argc, char **argv)
     // start GraphBLAS and LAGraph
     bool burble = true ;               // set true for diagnostic outputs
     demo_init (burble) ;
-
-    GrB_Vector rand_v = NULL, sort_r = NULL, set_v = NULL, assign_s = NULL;
-    GrB_Index *rand_a = NULL;
+    GrB_Matrix P = NULL;
+    GrB_Vector rand_v = NULL, sort_r = NULL, set_v = NULL, assign_s = NULL,
+        ramp_v = NULL;
+    GrB_Index *rand_a = NULL, *ramp = NULL;
     bool *set_a = NULL;
-    GrB_Index r_size = 0;
+    GrB_Index r_size = 0, ramp_size = 0, junk_size = 0;
     bool iso = false;
     LG_TRY (LAGraph_Random_Init (msg)) ;
     //--------------------------------------------------------------------------
@@ -55,20 +56,32 @@ int main (int argc, char **argv)
     // readproblem can read in a file in Matrix Market format, or in a binary
     // format created by binwrite (see LAGraph_demo.h, or the main program,
     // mtx2bin_demo).
-
+    bool *val_of_P = NULL;
     double t = LAGraph_WallClockTime ( ) ;
     GrB_Index size = (argc > 1) ? atoll(argv [1]) : 1000 ;
     int shift_e = __builtin_clzl(size);
     GrB_Index size_p2 = (1ull << (64-shift_e));
     GrB_Index bit_mask = size_p2 - 1;
     GRB_TRY (GrB_Vector_new(&rand_v, GrB_UINT64, size)) ;
+    GRB_TRY (GrB_Vector_new(&ramp_v, GrB_UINT64, size + 1)) ;
     GRB_TRY (GrB_Vector_new(&sort_r, GrB_UINT64, size)) ;
     GRB_TRY (GrB_Vector_new(&set_v, GrB_BOOL, size_p2)) ;
     GRB_TRY (GrB_Vector_new(&assign_s, GrB_BOOL, size_p2)) ;
+    GRB_TRY (GrB_Matrix_new(&P, GrB_BOOL, size_p2, size)) ;
 
+
+    LG_TRY (LAGraph_Malloc ((void**)(&val_of_P), 1, sizeof(bool), msg)) ;
+    val_of_P[0] = 1;
 
     GRB_TRY (GrB_Vector_assign_UINT64(
         rand_v, NULL, NULL, 0ull, GrB_ALL, 0, NULL)) ;
+    GRB_TRY (GrB_Vector_assign_UINT64(
+        ramp_v, NULL, NULL, 0ull, GrB_ALL, 0, NULL)) ;
+    GRB_TRY (GrB_Vector_apply_IndexOp_INT64(
+        ramp_v, NULL, NULL, GrB_ROWINDEX_INT64, ramp_v, 0, NULL)) ;
+    GRB_TRY (GxB_Vector_unpack_Full(
+        ramp_v, (void **)&ramp, &ramp_size, &iso, NULL
+    )) ;
     // GRB_TRY (GrB_Vector_assign_BOOL(
     //     assign_s, NULL, NULL, 0, GrB_ALL, 0, NULL)) ;
     GRB_TRY(GrB_set (assign_s, GxB_BITMAP, GxB_SPARSITY_CONTROL) ;)
@@ -119,9 +132,27 @@ int main (int argc, char **argv)
         assign_s, NULL, NULL, 1, rand_a, size, NULL)) ;
     t = LAGraph_WallClockTime ( ) - t ;
     printf ("Time for Assign: %g sec\n", t) ;
+    GRB_TRY (GrB_Vector_clear(assign_s)) ;
+    t = LAGraph_WallClockTime ( ) ;
+    GRB_TRY (GxB_Matrix_pack_CSC(
+        P, &ramp, &rand_a, (void**) &val_of_P, ramp_size,
+        r_size, sizeof(bool), true, false, NULL
+    ));
+    GRB_TRY (GrB_Matrix_reduce_Monoid(
+        assign_s, NULL, NULL, GxB_ANY_BOOL_MONOID, P, NULL
+    ));
+    GRB_TRY (GxB_Matrix_unpack_CSC(
+        P, &ramp, &rand_a, (void**) &val_of_P, &ramp_size,
+        &r_size, &junk_size, &iso, NULL, NULL
+    ));
+    t = LAGraph_WallClockTime ( ) - t ;
+    printf ("Time for CSR Magic: %g sec\n", t) ;
 
     GRB_TRY (GxB_Vector_pack_Full (
         rand_v, (void **)&rand_a, r_size, iso, NULL
+    )) ;
+    GRB_TRY (GxB_Vector_pack_Full (
+        ramp_v, (void **)&ramp, ramp_size, iso, NULL
     )) ;
     //--------------------------------------------------------------------------
     // check the results (make sure Y is a copy of G->A)
