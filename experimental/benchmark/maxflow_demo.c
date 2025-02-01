18e44144c64a55c99e16002fd4bdbd09b7fc83d9
dperry17 <darin.peries@gmail.com>
2025-02-01 17:22:37 -0600

    fixed benchmark file

--- experimental/benchmark/maxflow_demo.c
+++ experimental/benchmark/maxflow_demo.c
@@ -3,16 +3,15 @@
 #include "../../src/benchmark/LAGraph_demo.h"
 
 #undef LAGRAPH_CATCH
+#undef GRB_CATCH
 
 #define LAGRAPH_CATCH(info)                                                    \
   {                                                                            \
-    GrB_free(&R);                                                              \
     LAGraph_Delete(&G, msg);                                                   \
     return (info);                                                             \
   }
 
-#define LG_FREE_ALL                                                            \
-  { GrB_free(&R); }
+#define GRB_CATCH(info) LAGRAPH_CATCH(info)
 
 
 
@@ -20,23 +19,32 @@ int main (int argc, char ** argv){
 
   char msg[LAGRAPH_MSG_LEN];
   LAGraph_Graph G = NULL;
-  GrB_Matrix R = NULL;
+  
   double flow = 0;
   GrB_Index T=0, S=0;
 
   LAGRAPH_TRY(LAGraph_Init(msg));
   
   //read in graph
-  LAGRAPH_TRY(LAGraph_MMRead(&R, stdin, msg));
-  GRB_TRY(GrB_Matrix_nrows(&T, R));
-  T--;
+  double t = LAGraph_WallClockTime ( ) ;
+  char *matrix_name = (argc > 1) ? argv [1] : "stdin" ;
+  
   LAGRAPH_TRY(readproblem(&G, NULL, false, true, false, NULL, true, argc, argv));
+  
+  t = LAGraph_WallClockTime ( ) - t ;
+  printf ("Time to read the graph:      %g sec\n", t) ;
+
+  
+  GRB_TRY(GrB_Matrix_nrows(&T, G->A));
+  T--;
+
+  LG_SET_BURBLE(1);
   double time = LAGraph_WallClockTime();
   LAGRAPH_TRY(LAGraph_MaxFlow(G, S, T, &flow, msg));
   time = LAGraph_WallClockTime() - time;
 
   printf("Time for LAGraph_MaxFlow: %g sec\n", time);
-  GrB_free(&R);
+ 
   LAGraph_Delete(&G, msg);
   LAGRAPH_TRY(LAGraph_Finalize(msg));
 
