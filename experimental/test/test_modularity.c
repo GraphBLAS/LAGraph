4ee04ba7770a2cc0fa00a6794661d68a9c26adf0
Olumayowa <ooolowomeye@gmail.com>
2025-03-05 18:41:49 -0600

    fixed seg fault in modularity

--- experimental/test/test_modularity.c
+++ experimental/test/test_modularity.c
@@ -30,7 +30,6 @@ const matrix_info files[] = {
 void test_modularity (void)
 {
     LAGraph_Init (msg) ;
-    double *Q;
     for(int k = 0;;k++){
         if (strlen(files[k].matrix_file) == 0)
             break;
@@ -53,12 +52,13 @@ void test_modularity (void)
 
         GrB_Matrix B=NULL;
         double gamma = files[k].gamma;
-        OK(LAGr_Modularity2(Q,gamma,A,S,msg));
+        double Q;
+        OK(LAGr_Modularity2(&Q,gamma,A,S,msg));
         bool ok_mod =false;
         // GxB_print()
-        printf("Q:%.15g\n",*Q);
+        printf("Q:%.15g\n",Q);
         // printf("mod:%.15g\n",files[k].mod);
-        ok_mod =( *Q  - files[k].mod < 1e-5);
+        ok_mod =( Q  - files[k].mod < 1e-5);
         TEST_CHECK(ok_mod);
         OK (GrB_free (&A)) ;
         OK (GrB_free (&S)) ;
