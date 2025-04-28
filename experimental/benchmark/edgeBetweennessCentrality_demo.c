0b65549b0d4d3244ec79a6deec67adfa23cfd22b
peicasey <peicasey@gmail.com>
2025-04-28 02:18:44 -0500

    fix demo

--- experimental/benchmark/edgeBetweennessCentrality_demo.c
+++ experimental/benchmark/edgeBetweennessCentrality_demo.c
@@ -145,7 +145,7 @@ int main (int argc, char **argv)
 
     GrB_Matrix reference_centrality = NULL;
     t = LAGraph_WallClockTime ();
-    LAGRAPH_TRY (LG_check_edgeBetweennessCentrality(&reference_centrality, G, NULL, msg));
+    LAGRAPH_TRY (LG_check_edgeBetweennessCentrality(&reference_centrality, G, sources, msg));
     t = LAGraph_WallClockTime () - t;
     printf ("Time for LG_check_edgeBetweennessCentrality: %g sec\n", t);
 
