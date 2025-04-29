adcd31f75a849737e6578b3d4cb8ae8ca3e12fbd
Tim Davis <DrTimothyAldenDavis@gmail.com>
2025-04-29 11:51:16 -0500

    testing diameter and multibfs

--- experimental/algorithm/LAGraph_ExactDiameter.c
+++ experimental/algorithm/LAGraph_ExactDiameter.c
@@ -12,7 +12,6 @@
 //------------------------------------------------------------------------------
 
 // FIXME: almost ready for src; says it's not vanilla but should be OK
-// FIXME: need the CI test for this method
 
 // Takes in a graph and finds the diameter 
 // and optionally also the peripheral nodes of the graph
